a6fa768e65dfbb0e86bc87f2da49f8b1c459d0d1
Mike Edwards <mikeedwards83@googlemail.com>
2012-09-13 19:36:19 +0100

    Initial solution setup

---
+++ Tests/Integration Test/Glass.Mapper.Sc.Integration/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glass.Mapper.Sc.Integration
+{
+    public class Class1
+    {
+    }
+}
---
+++ Tests/Integration Tests/Glass.Mapper.Umb.Integration/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glass.Mapper.Umb.Integration
+{
+    public class Class1
+    {
+    }
+}
