a5d0e7a69022a4d390d4bab3e2a2e35b3cfb4871
Michael Edwards <mikeedwards83@googlemail.com>
2012-09-26 07:24:52 +0100

    CreeateIntefaceTask tests

---
+++ Tests/Unit Tests/Glass.Mapper.Tests/Pipelines/ObjectConstruction/Tasks/CreateInterface/CreateInterfaceTaskFixture.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glass.Mapper.Pipelines.ObjectConstruction;
+using Glass.Mapper.Pipelines.ObjectConstruction.Tasks.CreateInterface;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Tests.Pipelines.ObjectConstruction.Tasks.CreateInterface
+{
+    [TestFixture]
+    public class CreateInterfaceTaskFixture
+    {
+        private CreateInterfaceTask _task;
+
+        [SetUp]
+        public void Setup()
+        {
+            _task = new CreateInterfaceTask();
+        }
+
+        #region Method - Execute
+
+        [Test]
+        public void Execute_ConcreteClass_ObjectNotCreated()
+        {
+            //Assign
+            AbstractItemContext context = Substitute.For<AbstractItemContext>();
+            context.Type = typeof (StubClass);
+
+            ObjectConstructionArgs args = new ObjectConstructionArgs(context);
+
+            //Act
+            _task.Execute(args);
+
+            //Assert
+            Assert.IsNull(args.Object);
+            Assert.IsFalse(args.AbortPipeline);
+
+        }
+
+        [Test]
+        public void Execute_ProxyInterface_ProxyGetsCreated()
+        {
+            //Assign
+            AbstractItemContext context = Substitute.For<AbstractItemContext>();
+            context.Type = typeof(IStubInterface);
+
+            ObjectConstructionArgs args = new ObjectConstructionArgs(context);
+
+            //Act
+            _task.Execute(args);
+
+            //Assert
+            Assert.IsNotNull(args.Object);
+            Assert.IsTrue(args.AbortPipeline);
+            Assert.IsTrue(args.Object is IStubInterface);
+            Assert.IsFalse(args.Object.GetType() == typeof(IStubInterface));
+        }
+
+
+
+        #endregion
+
+        #region Stubs
+
+        public class StubClass
+        {
+
+        }
+
+        public interface IStubInterface
+        {
+
+        }
+
+        #endregion
+    }
+}
--- Tests/Unit Tests/Glass.Mapper.Tests/UtilitiesFixture.cs
+++ Tests/Unit Tests/Glass.Mapper.Tests/UtilitiesFixture.cs
@@ -15,7 +15,84 @@ namespace Glass.Mapper.Tests
         [Test]
         public void CreateConstructorDelegates_NoParameters_CreatesSingleConstructor()
         {
+            //Assign
+            Type type = typeof (StubNoParameters);
 
+            //Act
+            var result = Utilities.CreateConstructorDelegates(type);
+
+            //Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(0, result.First().Key.GetParameters().Count());
+        }
+
+        [Test]
+        public void CreateConstructorDelegates_OneParameters_CreatesSingleConstructor()
+        {
+            //Assign
+            Type type = typeof(StubOneParameter);
+
+            //Act
+            var result = Utilities.CreateConstructorDelegates(type);
+
+            //Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(1, result.First().Key.GetParameters().Count());
+        }
+
+        [Test]
+        public void CreateConstructorDelegates_TwoParameters_CreatesSingleConstructor()
+        {
+            //Assign
+            Type type = typeof(StubTwoParameters);
+
+            //Act
+            var result = Utilities.CreateConstructorDelegates(type);
+
+            //Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(2, result.First().Key.GetParameters().Count());
+        }
+
+        [Test]
+        public void CreateConstructorDelegates_ThreeParameters_CreatesSingleConstructor()
+        {
+            //Assign
+            Type type = typeof(StubThreeParameters);
+
+            //Act
+            var result = Utilities.CreateConstructorDelegates(type);
+
+            //Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(3, result.First().Key.GetParameters().Count());
+        }
+
+        [Test]
+        public void CreateConstructorDelegates_FourParameters_CreatesSingleConstructor()
+        {
+            //Assign
+            Type type = typeof(StubFourParameters);
+
+            //Act
+            var result = Utilities.CreateConstructorDelegates(type);
+
+            //Assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(4, result.First().Key.GetParameters().Count());
+        }
+
+        [Test]
+        [ExpectedException(typeof(MapperException))]
+        public void CreateConstructorDelegates_FiveParameters_CreatesSingleConstructor()
+        {
+            //Assign
+            Type type = typeof(StubFiveParameters);
+
+            //Act
+            var result = Utilities.CreateConstructorDelegates(type);
+
+            //Assert
         }
 
         #region Stubs
@@ -55,6 +132,14 @@ namespace Glass.Mapper.Tests
         public class StubFourParameters
         {
             public StubFourParameters(string param1, string param2, string param3, string param4)
+            {
+
+            }
+        }
+
+        public class StubFiveParameters
+        {
+            public StubFiveParameters(string param1, string param2, string param3, string param4, string param5)
             {
 
             }
