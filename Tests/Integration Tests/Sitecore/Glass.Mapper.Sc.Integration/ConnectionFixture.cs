00b487cd2fc2a3415c953164b0f76b15c2c31698
Michael Edwards <mikeedwards83@googlemail.com>
2012-10-07 18:09:40 +0100

    Sitecore integration test setup

---
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/ConnectionFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Sc.Integration
+{
+    [TestFixture]
+    public class ConnectionFixture
+    {
+        [Test]
+        public void ConnectionTest_ReadsSitecoreItem()
+        {
+            //Act
+            var db = Sitecore.Configuration.Factory.GetDatabase("master");
+            var result = db.GetItem("/sitecore");
+            
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("sitecore", result.Key);
+        }
+    }
+}
