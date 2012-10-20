aa8d212bafc2e295850843bd02c585b540930f69
Michael Edwards <mikeedwards83@googlemail.com>
2012-10-20 08:44:43 +0100

    extension method tests

---
+++ Tests/Unit Tests/Glass.Mapper.Tests/ExtensionMethodFixture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Tests
+{
+    [TestFixture]
+    public class ExtensionMethodFixture
+    {
+        #region Method - Formatted
+
+        [Test]
+        [Sequential]
+        public void Formatted_ParametersPassedToMethod_ReturnsCorrectlyFormattedString(
+            [Values("a", null, "a")] string param1,
+            [Values(null, "a", "b")] string param2,
+            [Values("{0}", "{1}", "{0} {1}")] string input,
+            [Values("a", "a", "a b")] string expected)
+        {
+            //Act
+            string result = ExtensionMethods.Formatted(input, param1, param2);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+
+        #region Method - IsNullOrEmpty
+
+        [Test]
+        [Sequential]
+        public void IsNullOrEmpty_TestSetOfValues_ReturnsExpectedResult(
+            [Values(null, "", "ab")] string value,
+            [Values(true, true, false)] bool expected)
+        {
+            //Act
+            var result = ExtensionMethods.IsNullOrEmpty(value);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+
+        #region Method - IsNotNullOrEmpty
+
+        [Test]
+        [Sequential]
+        public void IsNotNullOrEmpty_TestSetOfValues_ReturnsExpectedResult(
+            [Values(null, "", "ab")] string value,
+            [Values(false, false, true)] bool expected)
+        {
+            //Act
+            var result = ExtensionMethods.IsNotNullOrEmpty(value);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+
+       
+    }
+
+    
+}
