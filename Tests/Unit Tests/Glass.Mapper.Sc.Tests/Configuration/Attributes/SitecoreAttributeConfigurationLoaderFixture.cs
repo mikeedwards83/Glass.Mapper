c38f61b2aee37dc6f48ec89891d844ef309f4106
Michael Edwards <mikeedwards83@googlemail.com>
2012-10-21 09:32:06 +0100

    sitecoreAttributionConfigurationLoader created

---
+++ Tests/Unit Tests/Glass.Mapper.Sc.Tests/Configuration/Attributes/SitecoreAttributeConfigurationLoaderFixture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Sc.Tests.Configuration.Attributes
+{
+    [TestFixture]
+    public class SitecoreAttributeConfigurationLoaderFixture
+    {
+    }
+}
