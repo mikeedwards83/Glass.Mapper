168e5f2d6bc2c1e78519b800ecc98afa59d0d6ae
Michael Edwards <mikeedwards83@googlemail.com>
2012-10-27 21:44:22 +0100

    changes to Id attribute configuration

--- Source/Glass.Mapper.Sc/Configuration/Attributes/SitecoreIdAttribute.cs
+++ Source/Glass.Mapper.Sc/Configuration/Attributes/SitecoreIdAttribute.cs
@@ -10,5 +10,10 @@ namespace Glass.Mapper.Sc.Configuration.Attributes
     public class SitecoreIdAttribute : IdAttribute
     {
         public SitecoreIdAttribute():base(typeof(ID)) { }
+
+        public override Mapper.Configuration.AbstractPropertyConfiguration Configure(System.Reflection.PropertyInfo propertyInfo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
