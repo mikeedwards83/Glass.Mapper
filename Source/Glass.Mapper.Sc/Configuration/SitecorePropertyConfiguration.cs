6056dbbb2a6e059a0a0f49fcaf2197cc8a74bc3e
Michael Edwards <mikeedwards83@googlemail.com>
2012-11-24 15:14:48 +0000

    comment on SitecorePropertyConfiguration

--- Source/Glass.Mapper.Sc/Configuration/SitecorePropertyConfiguration.cs
+++ Source/Glass.Mapper.Sc/Configuration/SitecorePropertyConfiguration.cs
@@ -6,6 +6,7 @@ using Glass.Mapper.Configuration;
 
 namespace Glass.Mapper.Sc.Configuration
 {
+    //this might need to change to an interface
 	public class SitecorePropertyConfiguration : AbstractPropertyConfiguration
 	{
 	}
