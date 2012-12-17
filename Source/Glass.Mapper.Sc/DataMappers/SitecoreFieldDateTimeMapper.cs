79ac4355cbbd0ccfdc55c7deb3c461262de6b89b
Michael Edwards <mikeedwards83@googlemail.com>
2012-12-17 18:34:49 +0000

    SitecoreFieldDateTimeMapper

---
+++ Source/Glass.Mapper.Sc/DataMappers/SitecoreFieldDateTimeMapper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glass.Mapper.Sc.DataMappers
+{
+    public class SitecoreFieldDateTimeMapper : AbstractSitecoreFieldMapper
+    {
+        public SitecoreFieldDateTimeMapper():
+            base(typeof(DateTime))
+        {
+
+        }
+
+        public override object GetFieldValue(Sitecore.Data.Fields.Field field, Configuration.SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            return global::Sitecore.DateUtil.IsoDateToDateTime(field.Value);
+        }
+
+        public override void SetFieldValue(Sitecore.Data.Fields.Field field, object value, Configuration.SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            if (value is DateTime)
+            {
+                DateTime date = (DateTime)value;
+                field.Value = global::Sitecore.DateUtil.ToIsoDate(date);
+            }
+            else
+                throw new NotSupportedException("The value is not of type System.DateTime");
+        }
+    }
+}
---
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/DataMappers/SitecoreFieldDateTimeMapperFixture.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glass.Mapper.Sc.DataMappers;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Sc.Integration.DataMappers
+{
+    [TestFixture]
+    public class SitecoreFieldDateTimeMapperFixture : AbstractMapperFixture
+    {
+        #region Method - GetFieldValue
+
+        [Test]
+        public void GetFieldValue_FieldContainsValidDate_ReturnsDateTime()
+        {
+            //Assign
+            string fieldValue = "20120101T010101";
+            DateTime expected = new DateTime(2012,01,01,01,01,01);
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldDateTimeMapper/GetFieldValue");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldDateTimeMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = fieldValue;
+            }
+
+            //Act
+            var result = (DateTime) mapper.GetFieldValue(field, null, null);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+
+        #region Method - SetFieldValue
+
+        [Test]
+        public void SetFieldValue_DateTimePassed_SetsFieldValue()
+        {
+            //Assign
+            string expected = "20120101T010101";
+            DateTime objectValue = new DateTime(2012, 01, 01, 01, 01, 01);
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldDateTimeMapper/GetFieldValue");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldDateTimeMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = string.Empty;
+            }
+
+            //Act
+            using (new ItemEditing(item, true))
+            {
+                mapper.SetFieldValue(field, objectValue, null, null);
+            }
+        
+
+            //Assert
+            Assert.AreEqual(expected, field.Value);
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void SetFieldValue_NonDateTimePassed_ExceptionThrown()
+        {
+            //Assign
+            string expected = "20120101T010101";
+            int objectValue = 4; 
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldDateTimeMapper/GetFieldValue");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldDateTimeMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = string.Empty;
+            }
+
+            //Act
+            using (new ItemEditing(item, true))
+            {
+                mapper.SetFieldValue(field, objectValue, null, null);
+            }
+
+
+            //Assert
+        }
+
+        #endregion
+    }
+}
