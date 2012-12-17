c032979510fece5dc0cf7befd4c17ec5f49e7eba
Michael Edwards <mikeedwards83@googlemail.com>
2012-12-17 19:14:43 +0000

    SitecoreFieldDoubleMapper

---
+++ Source/Glass.Mapper.Sc/DataMappers/SitecoreFieldDoubleMapper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Glass.Mapper.Sc.Configuration;
+using Sitecore.Data.Fields;
+
+namespace Glass.Mapper.Sc.DataMappers
+{
+    public class SitecoreFieldDoubleMapper : AbstractSitecoreFieldMapper
+    {
+        public SitecoreFieldDoubleMapper() : base(typeof (double))
+        {
+
+        }
+
+        public override object GetFieldValue(Field field, SitecoreFieldConfiguration config,
+                                             SitecoreDataMappingContext context)
+        {
+            if (field.Value.IsNullOrEmpty()) return 0d;
+            double dValue = 0;
+            if (double.TryParse(field.Value, NumberStyles.Any, CultureInfo.InvariantCulture, out dValue)) return dValue;
+            else throw new MapperException("Could not convert value to double");
+        }
+
+        public override void SetFieldValue(Field field, object value, SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            if (value is double)
+            {
+                field.Value = value.ToString();
+            }
+            else
+                throw new NotSupportedException("The value is not of type System.Double");
+        }
+    }
+}
