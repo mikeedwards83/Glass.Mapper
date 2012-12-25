0fc74ea9f75ebd4eb9a1738e5c660c7c94b20f52
Michael Edwards <mikeedwards83@googlemail.com>
2012-12-25 11:57:06 +0000

    SitecoreFieldIntegerMapper

---
+++ Source/Glass.Mapper.Sc/DataMappers/SitecoreFieldIntegerMapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using Glass.Mapper.Sc.Configuration;
+using Sitecore.Data.Fields;
+
+namespace Glass.Mapper.Sc.DataMappers
+{
+    public class SitecoreFieldIntegerMapper : AbstractSitecoreFieldMapper
+    {
+        public SitecoreFieldIntegerMapper()
+            : base(typeof(double))
+        {
+
+        }
+
+        public override object GetFieldValue(string fieldValue, SitecoreFieldConfiguration config,
+                                             SitecoreDataMappingContext context)
+        {
+            if (fieldValue.IsNullOrEmpty()) return 0;
+            int dValue = 0;
+            if (int.TryParse(fieldValue, NumberStyles.Any, CultureInfo.InvariantCulture, out dValue)) return dValue;
+            else throw new MapperException("Could not convert value to double");
+        }
+
+        public override string SetFieldValue(object value, SitecoreFieldConfiguration config, SitecoreDataMappingContext context)
+        {
+            if (value is int)
+            {
+                return value.ToString();
+            }
+            else
+                throw new NotSupportedException("The value is not of type System.Double");
+        }
+    }
+}
---
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/DataMappers/SitecoreFieldIntegerMapperFixture.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Glass.Mapper.Sc.DataMappers;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Sc.Integration.DataMappers
+{
+    [TestFixture]
+    public  class SitecoreFieldIntegerMapperFixture : AbstractMapperFixture
+    {
+        #region Method - GetField
+
+        [Test]
+        public void GetField_FieldContainsValidInteger_ReturnsInteger()
+        {
+            //Assign
+            string fieldValue = "3";
+            int expected = 3;
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldIntegerMapper/GetField");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldIntegerMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = fieldValue;
+            }
+
+            //Act
+            var result = (int)mapper.GetField(field, null, null);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        public void GetField_FieldContainsEmptyString_ReturnsIntegerZero()
+        {
+            //Assign
+            string fieldValue = string.Empty;
+            int expected = 0;
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldIntegerMapper/GetField");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldIntegerMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = fieldValue;
+            }
+
+            //Act
+            var result = (int)mapper.GetField(field, null, null);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        [ExpectedException(typeof(MapperException))]
+        public void GetField_FieldContainsInvalidValidInteger_ReturnsInteger()
+        {
+            //Assign
+            string fieldValue = "hello world";
+            int expected = 3;
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldIntegerMapper/GetField");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldIntegerMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = fieldValue;
+            }
+
+            //Act
+            var result = (int)mapper.GetField(field, null, null);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        #endregion
+
+
+        #region Method - SetField
+
+        [Test]
+        public void SetField_ObjectisValidInteger_SetsFieldValue()
+        {
+            //Assign
+            string expected = "3";
+            int objectValue = 3;
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldIntegerMapper/SetField");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldIntegerMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = string.Empty;
+            }
+
+            //Act
+            using (new ItemEditing(item, true))
+            {
+                mapper.SetField(field, objectValue, null, null);
+            }
+
+
+            //Assert
+            Assert.AreEqual(expected, field.Value);
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotSupportedException))]
+        public void SetField_ObjectIsDouble_ThrowsException()
+        {
+            //Assign
+            double objectValue = 3;
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldIntegerMapper/SetField");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldIntegerMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = string.Empty;
+            }
+
+            //Act
+            using (new ItemEditing(item, true))
+            {
+                mapper.SetField(field, objectValue, null, null);
+            }
+
+
+            //Assert
+        }
+
+        #endregion
+    }
+}
