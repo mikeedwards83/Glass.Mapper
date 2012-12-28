b6244564c463e9e7843016df7abb43488702b617
Michael Edwards <mikeedwards83@googlemail.com>
2012-12-28 10:23:56 +0000

    SitecoreFieldNameValueCollectionMapper tests

--- Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/DataMappers/SitecoreFieldNameValueCollectionMapperFixture.cs
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/DataMappers/SitecoreFieldNameValueCollectionMapperFixture.cs
@@ -1,25 +1,118 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
+using Glass.Mapper.Sc.DataMappers;
 using NUnit.Framework;
+using Sitecore.Data;
 
 namespace Glass.Mapper.Sc.Integration.DataMappers
 {
     [TestFixture]
-    public class SitecoreFieldNameValueCollectionMapperFixture
+    public class SitecoreFieldNameValueCollectionMapperFixture : AbstractMapperFixture
     {
         #region Method - GetField
 
         [Test]
         public void GetField_FieldContainsThreeItems_ReturnsNameValueCollection()
         {
+            //Assign
+            var fieldValue = "Name1=Value1&Name2=Value2";
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldNameValueCollectionMapper/GetField");
+            var field = item.Fields[FieldName];
 
+            var mapper = new SitecoreFieldNameValueCollectionMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = fieldValue;
+            }
+
+            //Act
+            var result = mapper.GetField(field, null, null) as NameValueCollection;
+
+            //Assert
+            Assert.AreEqual("Value1", result["Name1"]);
+            Assert.AreEqual("Value2", result["Name2"]);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [Test]
+        public void GetField_FieldIsEmpty_ReturnsEmptyNameValueCollection()
+        {
+            //Assign
+            var fieldValue = "";
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldNameValueCollectionMapper/GetField");
+            var field = item.Fields[FieldName];
+
+            var mapper = new SitecoreFieldNameValueCollectionMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = fieldValue;
+            }
+
+            //Act
+            var result = mapper.GetField(field, null, null) as NameValueCollection;
+
+            //Assert
+            Assert.AreEqual(0, result.Count);
         }
 
 
         #endregion
         #region Method - SetField
+
+        [Test]
+        public void SetField_CollectionHas2Values_FieldContainsQueryString()
+        {
+            //Assign
+            var expected = "Name1=Value1&Name2=Value2";
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldNameValueCollectionMapper/SetField");
+            var field = item.Fields[FieldName];
+            var value = new NameValueCollection();
+            value.Add("Name1", "Value1");
+            value.Add("Name2", "Value2");
+            var mapper = new SitecoreFieldNameValueCollectionMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = string.Empty;
+            }
+
+            //Act
+            using (new ItemEditing(item, true))
+            {
+                mapper.SetField(field, value, null, null);
+            }
+            //Assert
+            Assert.AreEqual(expected, field.Value);
+        }
+
+        [Test]
+        public void SetField_CollectionHas0Values_FieldIsEmpty()
+        {
+            //Assign
+            var expected = "";
+            var item = Database.GetItem("/sitecore/content/Tests/DataMappers/SitecoreFieldNameValueCollectionMapper/SetField");
+            var field = item.Fields[FieldName];
+            var value = new NameValueCollection();
+            var mapper = new SitecoreFieldNameValueCollectionMapper();
+
+            using (new ItemEditing(item, true))
+            {
+                field.Value = string.Empty;
+            }
+
+            //Act
+            using (new ItemEditing(item, true))
+            {
+                mapper.SetField(field, value, null, null);
+            }
+            //Assert
+            Assert.AreEqual(expected, field.Value);
+        }
 
         #endregion
     }
