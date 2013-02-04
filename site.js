20bf4e8358fa8a5f801128da8c67ce7410ca2c67
Mike Edwards <mikeedwards83@googlemail.com>
2013-02-04 19:48:05 +0000

    trying to get github embeds working

--- site.js
+++ site.js
@@ -1,1 +1,19 @@
-﻿
+﻿(function($) {
+    $(function () {
+        $("pre.github").forEach(function () {
+
+            var $pre = $(this);
+            var sha = $pre.Attr("sha");
+            var commit = new GitHubCommit({
+                username: "mikeedwards83",
+                reponame: "Glass.Mapper",
+                fileSHA: sha
+            });
+
+            commit.fetch(function(content) {
+                $pre.html(content);
+            });
+        });
+
+    });
+})(jQuery);
