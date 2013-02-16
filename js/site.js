5752368ac7fe8d6a3d38bd15e4792af0adfa5788
Mike Edwards <mikeedwards83@googlemail.com>
2013-02-16 14:57:00 +0000

    embedded file from github

--- js/site.js
+++ js/site.js
@@ -1,18 +1,26 @@
-﻿(function($) {
+﻿(function ($) {
     $(function () {
+
         $("pre.github").each(function () {
 
             var $pre = $(this);
             $pre.addClass("prettyprint");
-            
+
             var sha = $pre.attr("sha");
+            var path = $pre.attr("file");
+            console.log(path);
+            var ref = $pre.attr("ref");
             var commit = new GitHubCommit({
                 username: "mikeedwards83",
                 reponame: "Glass.Mapper",
-                fileSHA: sha
+                fileSHA: sha,
+                filePath: path,
+                ref: ref
+
+
             });
 
-            commit.fetch(function(content) {
+            commit.fetch(function (content) {
                 $pre.html(content);
                 prettyPrint();
             });
