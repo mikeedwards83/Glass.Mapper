ba03e3dfeb4f2027cfb5b5f67510833576f284cd
Mike Edwards <mikeedwards83@googlemail.com>
2013-02-18 21:35:39 +0000

    SC tutorial 3

---
+++ docs/tutorial/sitecore/tutorial03/tutorial03.config.cs
@@ -0,0 +1,22 @@
+﻿using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Fluent;
+using Glass.Mapper.Sites.Sc.Models.Misc;
+
+namespace Glass.Mapper.Sites.Sc.Models.Config
+{
+    public class Misc
+    {
+        public static SitecoreFluentConfigurationLoader Load()
+        {
+            var loader = new SitecoreFluentConfigurationLoader();
+
+            var menuItem = loader.Add<MenuItem>();
+            menuItem.Field(x => x.Title).FieldName("Page Title");
+            menuItem.Info(x => x.Url).InfoType(SitecoreInfoType.Url);
+            menuItem.Children(x => x.Children);
+
+            return loader;
+        }
+    }
+}
+
---
+++ docs/tutorial/sitecore/tutorial03/tutorial03.menuitem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Glass.Mapper.Website.docs.tutorial.sitecore.tutorial03
+{
+    public class tutorial03
+    {
+    }
+}
