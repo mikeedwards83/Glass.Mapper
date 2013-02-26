ca368458224e9f3a45c7808c95f0eaf523aa1608
Tom Dudfield <tom@dudfield.co.uk>
2013-02-26 17:29:10 +0000

    changes

--- Tests/Integration Tests/Umbraco/Glass.Mapper.Umb.Integration/ConnectionFixture.cs
+++ Tests/Integration Tests/Umbraco/Glass.Mapper.Umb.Integration/ConnectionFixture.cs
@@ -2,81 +2,51 @@
 using Glass.Mapper.Umb.Integration.Helpers;
 using NUnit.Framework;
 using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.UnitOfWork;
 using Umbraco.Core.Services;
-using umbraco;
-using umbraco.cms.businesslogic;
-using umbraco.cms.businesslogic.cache;
 
 namespace Glass.Mapper.Umb.Integration
 {
-    [TestFixture, RequiresSTA]
-    public class ConnectionFixture : BaseDatabaseFactoryTest
+    [TestFixture]
+    public class ConnectionFixture
     {
-       /* [SetUp]
-        public virtual void Initialize()
-        {
-            TestHelper.SetupLog4NetForTests();
-            TestHelper.InitializeContentDirectories();
-
-            AppDomain.CurrentDomain.SetData("DataDirectory", TestHelper.CurrentAssemblyDirectory);
-
-            //if (RequiresDbSetup)
-                TestHelper.InitializeDatabase();
-
-            //// Resolution.Freeze();
-
-            ////NOTE: We are not constructing with the service context here because it is not required for these tests (currently)
-            //// if we do, this means that we have to initialized the RepositoryResolver too.
-            //ApplicationContext.Current = new ApplicationContext
-            //    {
-            //        IsReady = true,
-            //        //assign the db context
-            //        DatabaseContext = new DatabaseContext(new DefaultDatabaseFactory())
-            //    };
-
-        }
-
-        [TearDown]
-        public virtual void TearDown()
-        {
-            TestHelper.CleanContentDirectories();
-
-            ////reset the app context
-            //DatabaseContext.Database.Dispose();
-            //ApplicationContext.ApplicationCache.ClearAllCache();
-            //ApplicationContext.Current = null;
-            //// Resolution.IsFrozen = false;
-
-            //if (RequiresDbSetup)
-            //{
-                TestHelper.ClearDatabase();
-            //    SqlCeContextGuardian.CloseBackgroundConnection();
-            //}
-
-            AppDomain.CurrentDomain.SetData("DataDirectory", null);
-
-            Cache.ClearAllCache();
-
-            //UmbracoSettings.ResetSetters();
-        }*/
-
-        [Test] 
-        public void ConnectionTest_ReadsDictionaryItem()
-        {
-            var value1 = Dictionary.DictionaryItem.hasKey("KEY");
-            Assert.IsFalse(value1);
-
-            Dictionary.DictionaryItem.addKey("KEY", "VALUE");
-
-            var value2 = Dictionary.DictionaryItem.hasKey("KEY");
-            Assert.IsTrue(value2);
-        }
         [Test]
-        public void Get_Item_User_Property()
+        public void ConnectionTest()
         {
-            ContentService c= new ContentService();
-            var sd =c.GetById(0);
-            Assert.AreEqual(sd, null);
+
+            var unitOfWork = Global.CreateUnitOfWork();
+
+            var repoFactory = new RepositoryFactory();
+
+            var service = new ContentService(unitOfWork, repoFactory);
+            ContentTypeService cTypeService = new ContentTypeService(unitOfWork, repoFactory,
+                                                                     new ContentService(unitOfWork),
+                                                                     new MediaService(unitOfWork, repoFactory));
+
+
+            var contents = service.GetRootContent();
+
+            ContentType cType = new ContentType(-1);
+            cType.Name = "TestType";
+            cType.Alias = "TestType";
+            cType.Thumbnail = string.Empty;
+
+            cTypeService.Save(cType);
+
+            Console.WriteLine("Ctype " + cType.Id);
+            Assert.Greater(cType.Id, 0);
+
+            Content content = new Content("METest", -1, cType);
+            service.Save(content);
+
+            Assert.Greater(content.Id, 0);
+
+            var content2 = service.GetById(content.Id);
+            Assert.AreEqual(content.Name, content2.Name);
+
+
         }
     }
 }
