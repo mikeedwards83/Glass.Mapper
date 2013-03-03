892edfe2aad3ee2d0ee7b3eb6cc5695e63942c35
Mike Edwards <mikeedwards83@googlemail.com>
2013-03-03 12:46:22 +0000

    sc tutorial 08

---
+++ docs/tutorial/sitecore/tutorial08/tutorial08.global.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace Glass.Mapper.Sites.Sc
+{
+    public class Global : Sitecore.Web.Application
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var context = Glass.Mapper.Context.Create(new GlassConfig());
+            var attributes = new SitecoreAttributeConfigurationLoader("Glass.Mapper.Sites.Sc");
+
+            context.Load(
+                Models.Config.Landing.Load(),
+                Models.Config.Misc.Load(),
+                Models.Config.Content.Load(),
+                attributes
+                );
+        }
+    }
+}
