f5a7f7924ef64d38d4c5ae26ec79b0fc20d4da5e
Mike Edwards <mikeedwards83@googlemail.com>
2013-03-18 20:05:50 +0000

    SCTutorial 01 updating due to code changes

---
+++ docs/tutorial/sitecore/tutorial01/tutorial01.app_start.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Glass.Mapper.Sc.CastleWindsor;
+using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace Glass.Mapper.Sites.Sc.App_Start
+{
+    public static class GlassMapper
+    {
+        [assembly: WebActivator.PreApplicationStartMethod(typeof(Glass.Mapper.Sites.Sc.App_Start.GlassMapper), "Start")]
+        public static void Start()
+        {
+            //create the resolver
+            var resolver = DependencyResolver.CreateStandardResolver();
+
+            //create a context
+            var context = Glass.Mapper.Context.Create(resolver);
+
+            var attributes = new SitecoreAttributeConfigurationLoader("Glass.Mapper.Sites.Sc");
+
+            context.Load(
+                attributes
+                );
+        }
+    }
+}
