985487fbae56dcee9a9ffad7110af39e1902e6e8
Mike Edwards <mikeedwards83@googlemail.com>
2013-03-19 07:20:31 +0000

    SCtutotial04 update

--- docs/tutorial/sitecore/tutorial04/tutorial04.newsarticle.cs
+++ docs/tutorial/sitecore/tutorial04/tutorial04.newsarticle.cs
@@ -1,20 +1,29 @@
 ﻿using System;
+using Glass.Mapper.Sc.Configuration;
+using Glass.Mapper.Sc.Configuration.Attributes;
 using Glass.Mapper.Sc.Fields;
 
 namespace Glass.Mapper.Sites.Sc.Models.Content
 {
-    public class NewsArticle
+    [SitecoreType]
+    public class NewsArticle 
     {
+        [SitecoreField]
         public virtual string Title { get; set; }
 
+        [SitecoreField]
         public virtual string Abstract { get; set; }
 
+        [SitecoreField]
         public virtual string MainBody { get; set; }
 
+        [SitecoreField]
         public virtual Image FeaturedImage { get; set; }
 
+        [SitecoreField]
         public virtual DateTime Date { get; set; }
 
+        [SitecoreInfo(SitecoreInfoType.Url)
         public virtual string Url { get; set; }
     }
 }
