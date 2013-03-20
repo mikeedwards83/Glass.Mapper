3476ff35824ae6ccdb408f645348271df9472a72
Michael Edwards <mikeedwards83@googlemail.com>
2013-03-20 07:25:03 +0000

    Tutorial Site Update

---
+++ Sites/Sitecore/Glass.Mapper.Sites.Sc/App_Start/GlassMapperSc.cs
@@ -0,0 +1,27 @@
+using System;
+using Glass.Mapper.Sc.CastleWindsor;
+using Glass.Mapper.Sc.Configuration.Attributes;
+
+[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Glass.Mapper.Sites.Sc.App_Start.GlassMapperSc), "Start")]
+
+namespace Glass.Mapper.Sites.Sc.App_Start
+{
+    public static class  GlassMapperSc
+    {
+        public static void Start()
+        {
+            //create the resolver
+            var resolver = DependencyResolver.CreateStandardResolver();
+
+            //create a context
+            var context = Glass.Mapper.Context.Create(resolver);
+
+            var attributes = new SitecoreAttributeConfigurationLoader("Glass.Mapper.Sites.Sc");
+
+            context.Load(
+                attributes,
+                Models.Config.ContentConfig.Load()
+                );
+        }
+    }
+}
