3ca9d2f9fc35e929f400a06a2660695a75118ea8
Michael Edwards <michael.edwards@eduserv.org.uk>
2013-04-08 13:09:28 +0100

    SC: updated tutorial 1

--- docs/tutorial/sitecore/tutorial01/tutorial01.app_start.cs
+++ docs/tutorial/sitecore/tutorial01/tutorial01.app_start.cs
@@ -10,16 +10,19 @@ namespace Glass.Mapper.Sites.Sc.App_Start
     {
         public static void Start()
         {
+            var config = GlassMapperScCustom.GetConfig();
+
             //create the resolver
-            var resolver = DependencyResolver.CreateStandardResolver();
+            var resolver = DependencyResolver.CreateStandardResolver(config);
+
+            //install the custom services
+            var container = (resolver as DependencyResolver).Container;
+            GlassMapperScCustom.CastleConfig(container, config);
 
             //create a context
             var context = Glass.Mapper.Context.Create(resolver);
-
-            var attributes = new SitecoreAttributeConfigurationLoader("Glass.Mapper.Sites.Sc");
-
             context.Load(
-                attributes
+                GlassMapperScCustom.GlassLoaders()
                 );
         }
     }
