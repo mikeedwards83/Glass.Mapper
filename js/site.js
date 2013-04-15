c75a8d648d696bbdd8258cb83457a35b44b34819
Mike Edwards <mikeedwards83@googlemail.com>
2013-04-15 22:24:30 +0100

    search page improvements

--- js/site.js
+++ js/site.js
@@ -30,3 +30,15 @@
         prettyPrint();
     });
 })(jQuery);
+
+//utilities
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if (results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
