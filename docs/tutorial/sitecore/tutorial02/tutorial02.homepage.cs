85e5e0f072641b61afa7228726a6d2b838aec4f5
Mike Edwards <mikeedwards83@googlemail.com>
2013-04-20 21:16:32 +0100

    SC Tutorial 2 Update

--- docs/tutorial/sitecore/tutorial02/tutorial02.homepage.cs
+++ docs/tutorial/sitecore/tutorial02/tutorial02.homepage.cs
@@ -2,12 +2,9 @@
 
 namespace Glass.Mapper.Sites.Sc.Models.Landing
 {
-
-    [SitecoreType(AutoMap = true)]
     public class HomePage
     {
         public virtual string Title { get; set; }
         public virtual string MainBody { get; set; }
     }
-
 }
