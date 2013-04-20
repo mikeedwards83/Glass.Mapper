0439fd9773fae395f4cfbc309cf1d3126987b912
Michael Edwards <mikeedwards83@googlemail.com>
2013-04-20 19:19:55 +0100

    Glass.Mapper.Sc.Sites changes

---
+++ Sites/Sitecore/Glass.Mapper.Sites.Sc/Models/Landing/BlogLanding.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Glass.Mapper.Sites.Sc.Models.Content;
+using Glass.Mapper.Sites.Sc.Models.Misc;
+
+namespace Glass.Mapper.Sites.Sc.Models.Landing
+{
+    public class BlogLanding: ContentBase
+    {
+        public virtual string MainBody { get; set; }
+        public virtual IEnumerable<BlogPost> Posts { get; set; }
+    }
