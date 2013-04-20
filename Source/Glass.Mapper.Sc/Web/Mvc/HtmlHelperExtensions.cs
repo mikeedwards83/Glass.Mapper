be3825a63b1c0fe0f86bf78951c265b32af1c95c
Michael Edwards <mikeedwards83@googlemail.com>
2013-04-20 19:59:56 +0100

    SC: namespace problem in HtmlHelperExtensions

--- Source/Glass.Mapper.Sc/Web/Mvc/HtmlHelperExtensions.cs
+++ Source/Glass.Mapper.Sc/Web/Mvc/HtmlHelperExtensions.cs
@@ -10,53 +10,50 @@ using Sitecore.Web.UI.WebControls;
 
 namespace Glass.Mapper.Sc.Web.Mvc
 {
-    namespace Glass.Sitecore.Mapper.Web.Mvc
+    public static class HtmlHelperExtensions
     {
-        public static class HtmlHelperExtensions
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper)
         {
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper)
-            {
-                return BeginEditFrame(htmlHelper, "");
-            }
+            return BeginEditFrame(htmlHelper, "");
+        }
 
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, ID dataSource)
-            {
-                return BeginEditFrame(htmlHelper, dataSource.ToString());
-            }
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, ID dataSource)
+        {
+            return BeginEditFrame(htmlHelper, dataSource.ToString());
+        }
 
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, string dataSource)
-            {
-                return BeginEditFrame(htmlHelper, dataSource, GlassEditFrame.DefaultEditButons);
-            }
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, string dataSource)
+        {
+            return BeginEditFrame(htmlHelper, dataSource, GlassEditFrame.DefaultEditButons);
+        }
 
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, ID dataSource, ID buttons)
-            {
-                return BeginEditFrame(htmlHelper, dataSource.ToString(), buttons.ToString());
-            }
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, ID dataSource, ID buttons)
+        {
+            return BeginEditFrame(htmlHelper, dataSource.ToString(), buttons.ToString());
+        }
 
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, ID dataSource, string buttons)
-            {
-                return BeginEditFrame(htmlHelper, dataSource.ToString(), buttons);
-            }
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, ID dataSource, string buttons)
+        {
+            return BeginEditFrame(htmlHelper, dataSource.ToString(), buttons);
+        }
 
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, string dataSource, ID buttons)
-            {
-                return BeginEditFrame(htmlHelper, dataSource, buttons.ToString());
-            }
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, string dataSource, ID buttons)
+        {
+            return BeginEditFrame(htmlHelper, dataSource, buttons.ToString());
+        }
 
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, string dataSource, string buttons)
-            {
-                var frame = new GlassEditFrame(buttons, htmlHelper.ViewContext.Writer, dataSource);
-                frame.RenderFirstPart();
-                return frame;
-            }
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, string dataSource, string buttons)
+        {
+            var frame = new GlassEditFrame(buttons, htmlHelper.ViewContext.Writer, dataSource);
+            frame.RenderFirstPart();
+            return frame;
+        }
 
-            public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, EditFrame editFrame)
-            {
-                var writter = new HtmlTextWriter(htmlHelper.ViewContext.Writer);
-                editFrame.RenderFirstPart(writter);
-                return new GlassEditFrame(editFrame);
-            }
+        public static GlassEditFrame BeginEditFrame(this HtmlHelper htmlHelper, EditFrame editFrame)
+        {
+            var writter = new HtmlTextWriter(htmlHelper.ViewContext.Writer);
+            editFrame.RenderFirstPart(writter);
+            return new GlassEditFrame(editFrame);
         }
     }
 }
