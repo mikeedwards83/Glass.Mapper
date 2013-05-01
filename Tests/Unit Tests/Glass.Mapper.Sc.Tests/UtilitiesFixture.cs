ce36f18e8207e6c9c6ec43ee206a30335952c813
Michael Edwards <mikeedwards83@googlemail.com>
2013-05-01 21:45:51 +0100

    Unit tests for get and set property functions

---
+++ Tests/Unit Tests/Glass.Mapper.Sc.Tests/UtilitiesFixture.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Sc.Tests
+{
+    [TestFixture]
+    public class UtilitiesFixture
+    {
+        [Test]
+        public void GetPropertyFunc_UsingPropertyInfo_ReturnsFuction(
+            [Values(
+                "GetPublicSetPublic",
+                "GetProtectedSetProtected",
+                "GetPrivateSetPrivate",
+                "GetPublicSetProtected",
+                "GetPublicSetPrivate",
+                "GetPublicNoSet"
+                )] string propertyName
+        )
+        {
+            //Assign
+            PropertyInfo info = typeof(StubClass).GetProperty(propertyName, Utilities.Flags);
+            Assert.IsNotNull(info);
+
+            //Act
+            var result = Utilities.GetPropertyFunc(info);
+
+            //Assert
+            Assert.IsNotNull(result);
+            
+        }
+
+        [Test]
+        public void SetPropertyAction_UsingPropertyInfo_ReturnsFuction(
+            [Values(
+                "GetPublicSetPublic",
+                "GetProtectedSetProtected",
+                "GetPrivateSetPrivate",
+                "GetPublicSetProtected",
+                "GetPublicSetPrivate",
+                "GetPublicNoSet"
+                )] string propertyName
+        )
+        {
+            //Assign
+            PropertyInfo info = typeof(StubClass).GetProperty(propertyName, Utilities.Flags);
+            Assert.IsNotNull(info);
+
+            //Act
+            var result = Utilities.SetPropertyAction(info);
+
+            //Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [Test]
+        public void GetPropertyFuncAndSetPropertyAction_ReadWriteUsingActionAndFunction_ReturnsString(
+            [Values(
+                "GetPublicSetPublic",
+                "GetProtectedSetProtected",
+                "GetPrivateSetPrivate",
+                "GetPublicSetProtected",
+                "GetPublicSetPrivate",
+                "GetPublicNoSet"
+                )] string propertyName
+        )
+        {
+            //Assign
+            PropertyInfo info = typeof(StubClass).GetProperty(propertyName, Utilities.Flags);
+
+            var getFunc = Utilities.GetPropertyFunc(info);
+            var setActi = Utilities.SetPropertyAction(info);
+            var expected = "some value";
+            var stub = new StubClass();
+
+            //Act
+            setActi(stub, expected);
+            var result = getFunc(stub) as string;
+
+            //Assert
+            Assert.AreEqual(expected, result);
+
+        }
+
+
+        #region Stubs
+
+        public class StubClass
+        {
+            public string GetPublicSetPublic { get; set; }
+            protected string GetProtectedSetProtected { get; set; }
+            private string GetPrivateSetPrivate { get; set; }
+            
+            public string GetPublicSetProtected { get; protected set; }
+            public string GetPublicSetPrivate { get; private set; }
+
+            private string _getPublicNoSet;
+            public string GetPublicNoSet
+            {
+                get { return _getPublicNoSet; }
+            }
+        }
+
+        #endregion
+
+    }
+}
