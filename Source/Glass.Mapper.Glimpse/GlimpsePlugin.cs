4239ea1a65bc1c7b418c454e5a7939255f705468
Michael Edwards <mie@sitecore.net>
2013-07-25 08:50:53 +0100

    Glimpse changes

---
+++ Source/Glass.Mapper.Glimpse/GlimpsePlugin.cs
@@ -0,0 +1,56 @@
+﻿using Glimpse.Core.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glass.Mapper.Glimpse
+{
+    public class GlimpsePlugin : ITab
+    {
+        public string Name
+        {
+            get { return "Glass.Mapper"; }
+        }
+
+        public object GetData(ITabContext context)
+        {
+            return GetContexts();
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
+
+        public Type RequestContextType
+        {
+            get { return null; }
+        }
+
+        private object GetContexts()
+        {
+            var contextDatas = new List<object>();
+
+            foreach (var context in Glass.Mapper.Context.Contexts)
+            {
+                var contextData = new
+                {
+                    ContextName = context.Value.Name,
+                    Configurations = context.Value.TypeConfigurations.Select(x=>
+                        new{
+                            Name = x.Value.Type.FullName,
+                            Properties = x.Value.Properties.Select(y=>new {
+                                Name = y.PropertyInfo.Name,
+                                Mapper = y.Mapper.GetType().FullName
+                            }),
+                            AutoMapped =x.Value.AutoMap
+                        })
+                };
+                contextDatas.Add(contextData);
+            }
+            return contextDatas;
+        }
+    }
+}
