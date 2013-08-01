724d0507e20f1f477606beda5cbcdbf18f7137a0
Michael Edwards <mikeedwards83@googlemail.com>
2013-08-01 22:28:44 +0100

    resharper cleanup

--- Source/Glass.Mapper/AbstractDataMapper.cs
+++ Source/Glass.Mapper/AbstractDataMapper.cs
@@ -17,10 +17,6 @@
 //-CRE-
 
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Glass.Mapper.Configuration;
 using Glass.Mapper.Pipelines.DataMapperResolver;
 
