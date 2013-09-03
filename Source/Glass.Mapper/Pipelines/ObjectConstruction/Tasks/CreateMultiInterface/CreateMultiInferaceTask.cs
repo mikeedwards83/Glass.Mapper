af0cddcd59c40e2633555cd2d49d7a669f6c94fa
Mike Edwards <mikeedwards83@googlemail.com>
2013-09-03 07:52:05 +0100

    completion of MulitInterface task an interceptor with unit tests

--- Source/Glass.Mapper/Pipelines/ObjectConstruction/Tasks/CreateMultiInterface/CreateMultiInferaceTask.cs
+++ Source/Glass.Mapper/Pipelines/ObjectConstruction/Tasks/CreateMultiInterface/CreateMultiInferaceTask.cs
@@ -1,24 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Glass.Mapper.Pipelines.ObjectConstruction.Tasks.CreateInterface;
+﻿using System.Linq;
+using System.Net.Mail;
+using Castle.DynamicProxy;
 
 namespace Glass.Mapper.Pipelines.ObjectConstruction.Tasks.CreateMultiInterface
 {
     public class CreateMultiInferaceTask : IObjectConstructionTask
     {
+        private static volatile ProxyGenerator _generator;
+
+              /// <summary>
+        /// Initializes static members of the <see cref="CreateInterfaceTask"/> class.
+        /// </summary>
+        static CreateMultiInferaceTask()
+        {
+            _generator = new ProxyGenerator();
+        }
+
         public void Execute(ObjectConstructionArgs args)
         {
             if (args.Result != null)
                 return;
 
-            //if (args.Configurations.First().Type.IsInterface && args.Configurations.Count() == 1)
-            //{
-
-            //    args.Result = _generator.CreateInterfaceProxyWithoutTarget(args.Configurations.First().Type, new InterfacePropertyInterceptor(args));
-            //    args.AbortPipeline();
-            //}
+            if (args.Configurations.All(x=>x.Type.IsInterface) && args.Configurations.Count() > 1)
+            {
+                args.Result = _generator.CreateInterfaceProxyWithoutTarget(
+                    args.Configurations.First().Type,
+                    args.Configurations.Skip(1).Select(x=>x.Type).ToArray(),
+                    new MultiInterfacePropertyInterceptor(args));
+            }
         }
     }
 }
