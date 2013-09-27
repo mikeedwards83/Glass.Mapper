35b2f08f7d7e2b24005a08b885b4c1ec4f6a22bb
Mike Edwards <mikeedwards83@googlemail.com>
2013-09-27 08:04:05 +0100

    fix for issue 27

--- Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/SitecoreServiceFixture.cs
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/SitecoreServiceFixture.cs
@@ -1195,6 +1195,46 @@ namespace Glass.Mapper.Sc.Integration
         }
 
         [Test]
+        public void Create_UsingInterfaceAndName_CreatesANewItem()
+        {
+            //Assign
+            string parentPath = "/sitecore/content/Tests/SitecoreService/Create";
+            string childPath = "/sitecore/content/Tests/SitecoreService/Create/newChild";
+            string name = "newChild";
+
+            var db = Sitecore.Configuration.Factory.GetDatabase("master");
+            var context = Context.Create(Utilities.CreateStandardResolver());
+            context.Load(new SitecoreAttributeConfigurationLoader("Glass.Mapper.Sc.Integration"));
+            var service = new SitecoreService(db);
+
+            using (new SecurityDisabler())
+            {
+                var parentItem = db.GetItem(parentPath);
+                parentItem.DeleteChildren();
+            }
+
+            var parent = service.GetItem<StubClass>(parentPath);
+            
+            //Act
+            StubClass child = null;
+            using (new SecurityDisabler())
+            {
+               child= service.Create<StubClass, StubClass>(parent, "newChild");
+            }
+
+            //Assert
+            var newItem = db.GetItem(childPath);
+
+            using (new SecurityDisabler())
+            {
+                newItem.Delete();
+            }
+
+            Assert.AreEqual(child.Name, newItem.Name);
+            Assert.AreEqual(child.Id, newItem.ID.Guid);
+        }
+
+        [Test]
         public void Create_AutoMappedClass_CreatesANewItem()
         {
             //Assign
