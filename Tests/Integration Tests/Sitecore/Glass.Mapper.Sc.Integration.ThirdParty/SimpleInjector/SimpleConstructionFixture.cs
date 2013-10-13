f75156dd04407570f75b41a9f8f79a67cc112353
Michael Edwards <mikeedwards83@googlemail.com>
2013-10-13 20:33:25 +0100

    simpleinjector tests for lazy loading

---
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration.ThirdParty/SimpleInjector/SimpleConstructionFixture.cs
@@ -0,0 +1,140 @@
+﻿using Castle.MicroKernel.Registration;
+using Glass.Mapper.Pipelines.ObjectConstruction;
+using Glass.Mapper.Sc.CastleWindsor;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using NUnit.Framework;
+using SimpleInjector;
+
+namespace Glass.Mapper.Sc.Integration.ThirdParty.SimpleInjector
+{
+    [TestFixture]
+    public class SimpleInjectorConstructionFixture
+    {
+        [Test]
+        public void Execute_RequestInstanceOfClassWithServiceButLazy_ReturnsInstanceWithService()
+        {
+            //Assign
+
+            SimpleInjectorTask.Container = new Container();
+            SimpleInjectorTask.Container.Register<StubServiceInterface, StubService>(); 
+            
+            var path = "/sitecore/content/Tests/ThirdParties/SimpleInjector/Target";
+            var resolver = Utilities.CreateStandardResolver(false, new[] { Component.For<IObjectConstructionTask>().ImplementedBy<SimpleInjectorTask>().LifestyleTransient() }) as DependencyResolver;
+
+            var context = Context.Create(resolver);
+            var db = Sitecore.Configuration.Factory.GetDatabase("master");
+            var service = new SitecoreService(db, context);
+            
+            var item = db.GetItem(path);
+            
+            string field1Expected = "hello world";
+            string field2 = item.ID.ToString();
+
+            using (new ItemEditing(item, true))
+            {
+                item["Field1"] = field1Expected;
+                item["Field2"] = field2;
+            }
+
+            
+            //Act
+            var result = service.GetItem<StubClassWithService>(path, true);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result is StubClassWithService);
+            Assert.AreNotEqual(typeof(StubClassWithService), result.GetType());
+
+            var stub = result as StubClassWithService;
+
+
+            Assert.AreEqual(field1Expected, stub.Field1);
+            Assert.IsNotNull(stub.Service);
+            Assert.IsTrue(stub.Service is StubService);
+
+
+            var field2Class = stub.Field2;
+            Assert.AreEqual(field1Expected, field2Class.Field1);
+            Assert.IsNotNull(field2Class.Service);
+            Assert.IsTrue(field2Class.Service is StubService);
+
+
+        }
+
+        [Test]
+        public void Execute_RequestInstanceOfClassWithServiceNotLazy_ReturnsInstanceWithService()
+        {
+            //Assign
+
+           SimpleInjectorTask.Container = new Container();
+            SimpleInjectorTask.Container.Register<StubServiceInterface, StubService>(); 
+
+            var path = "/sitecore/content/Tests/ThirdParties/SimpleInjector/Target";
+            var resolver = Utilities.CreateStandardResolver(false, new[] { Component.For<IObjectConstructionTask>().ImplementedBy<SimpleInjectorTask>().LifestyleTransient() }) as DependencyResolver;
+            var context = Context.Create(resolver);
+
+            var db = Sitecore.Configuration.Factory.GetDatabase("master");
+            var service = new SitecoreService(db, context);
+
+            var item = db.GetItem(path);
+
+            string field1Expected = "hello world";
+            string field2 = item.ID.ToString();
+
+            using (new ItemEditing(item, true))
+            {
+                item["Field1"] = field1Expected;
+                item["Field2"] = field2;
+            }
+
+
+            //Act
+            var result = service.GetItem<StubClassWithService>(path);
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result is StubClassWithService);
+            Assert.AreEqual(typeof(StubClassWithService), result.GetType());
+
+            var stub = result as StubClassWithService;
+
+
+            Assert.AreEqual(field1Expected, stub.Field1);
+            Assert.IsNotNull(stub.Service);
+            Assert.IsTrue(stub.Service is StubService);
+
+
+            var field2Class = stub.Field2;
+            Assert.AreEqual(field1Expected, field2Class.Field1);
+            Assert.IsNotNull(field2Class.Service);
+            Assert.IsTrue(field2Class.Service is StubService);
+        }
+
+        #region Stubs
+        public interface StubServiceInterface
+        {
+
+        }
+        public class StubService : StubServiceInterface
+        {
+            public string DoWork()
+            {
+                return "Work Done";
+            }
+
+        }
+        public class StubClassWithService
+        {
+            public StubServiceInterface Service { get; set; }
+
+            public StubClassWithService(StubServiceInterface service)
+            {
+               Service = service;
+            }
+
+            public virtual string Field1 { get; set; }
+            public virtual StubClassWithService Field2 { get; set; }
+        }
+        #endregion
+    }
+}
---
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration.ThirdParty/SimpleInjector/SimpleInjectorTask.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using Glass.Mapper.Sc.CastleWindsor.Pipelines.ObjectConstruction;
+using SimpleInjector;
+
+namespace Glass.Mapper.Sc.Integration.ThirdParty.SimpleInjector
+{
+    public class SimpleInjectorTask : Glass.Mapper.Pipelines.ObjectConstruction.IObjectConstructionTask
+    {
+        public static Container Container { get;  set; }
+        private static object _lock = new object();
+
+         private static volatile  ProxyGenerator _generator;
+        private static volatile  ProxyGenerationOptions _options;
+
+        /// <summary>
+        /// Initializes static members of the <see cref="CreateConcreteTask"/> class.
+        /// </summary>
+        static SimpleInjectorTask()
+        {
+            _generator = new ProxyGenerator();
+            var hook = new Glass.Mapper.Pipelines.ObjectConstruction.Tasks.CreateConcrete.LazyObjectProxyHook();
+            _options = new ProxyGenerationOptions(hook);
+        }
+
+        public void Execute(Glass.Mapper.Pipelines.ObjectConstruction.ObjectConstructionArgs args)
+        {
+            //check that no other task has created an object
+            //also check that this is a dynamic object
+            if (args.Result == null && !args.Configuration.Type.IsAssignableFrom(typeof(IDynamicMetaObjectProvider)))
+            {
+                //check to see if the type is registered with the SimpleInjector container
+                //if it isn't added it
+                if (Container.GetRegistration(args.Configuration.Type) == null)
+                {
+                    lock (_lock)
+                    {
+                        if (Container.GetRegistration(args.Configuration.Type) == null)
+                        {
+                            Container.Register(args.Configuration.Type);
+                        }
+                    }
+
+                 
+                }
+
+                 Action<object> mappingAction = (target) => 
+                        args.Configuration.MapPropertiesToObject(target, args.Service, args.AbstractTypeCreationContext);
+
+
+                if (args.AbstractTypeCreationContext.IsLazy)
+                {
+                    var parameters = args.Configuration.ConstructorMethods.Select(x => x.Key.GetParameters())
+                        .OrderBy(x => x.Length)
+                        .First();
+
+                    var resolved = parameters.Select(x => Container.GetInstance(x.ParameterType));
+                    
+                    var proxy = _generator.CreateClassProxy(args.Configuration.Type, resolved.ToArray(), new LazyObjectInterceptor( mappingAction));
+                    args.Result = proxy;
+                }
+                else
+                {
+                    //create instance using SimpleInjector
+                    var obj = Container.GetInstance(args.Configuration.Type);
+
+                    //map properties from item to model
+                    mappingAction(obj);
+
+                    //set the new object as the returned result
+                    args.Result = obj;
+                }
+            }
+        }
+    }
+}
