d3b8ee3134adb6303c144af95597ccdb5204060b
Michael Edwards <mikeedwards83@googlemail.com>
2013-10-17 19:03:45 +0100

    Tests for lazy IOC using SimpleInjector

---
+++ Source/Glass.Mapper/Pipelines/ObjectConstruction/Tasks/Ioc/IocTaskBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using Castle.DynamicProxy;
+using Glass.Mapper.Configuration;
+
+namespace Glass.Mapper.Pipelines.ObjectConstruction.Tasks.Ioc
+{
+    public abstract class IocTaskBase : Glass.Mapper.Pipelines.ObjectConstruction.IObjectConstructionTask
+    {
+        private static object _lock = new object();
+        private static volatile ProxyGenerator _generator;
+        private static volatile ProxyGenerationOptions _options;
+
+        protected static ProxyGenerator Generator { get { return _generator; } }
+        protected static ProxyGenerationOptions Options { get { return _options; } }
+
+        static IocTaskBase()
+        {
+            _generator = new ProxyGenerator();
+            var hook = new Glass.Mapper.Pipelines.ObjectConstruction.Tasks.CreateConcrete.LazyObjectProxyHook();
+            _options = new ProxyGenerationOptions(hook);
+        }
+
+        public void Execute(Glass.Mapper.Pipelines.ObjectConstruction.ObjectConstructionArgs args)
+        {
+            //check that no other task has created an object
+            //also check that this is a dynamic object
+            if (args.Result == null && !args.Configuration.Type.IsAssignableFrom(typeof(IDynamicMetaObjectProvider)))
+            {
+                //check to see if the type is registered with the SimpleInjector container
+                //if it isn't added it
+                if (IsRegistered(args.Configuration.Type))
+                {
+                    lock (_lock)
+                    {
+                        if (IsRegistered(args.Configuration.Type))
+                        {
+                            Register(args.Configuration.Type);
+                        }
+                    }
+
+
+                }
+
+                Action<object> mappingAction = (target) =>
+                       args.Configuration.MapPropertiesToObject(target, args.Service, args.AbstractTypeCreationContext);
+
+
+                if (args.AbstractTypeCreationContext.IsLazy)
+                {
+                    var resolved = GetConstructorParameters(args.Configuration);
+
+                    var proxy = _generator.CreateClassProxy(args.Configuration.Type, resolved, new LazyObjectInterceptor(mappingAction));
+                    args.Result = proxy;
+                }
+                else
+                {
+                    //create instance using SimpleInjector
+                    var obj = CreateConcreteInstance(args.Configuration);
+                    //map properties from item to model
+                    mappingAction(obj);
+
+                    //set the new object as the returned result
+                    args.Result = obj;
+                }
+            }
+        }
+
+        protected abstract bool IsRegistered(Type type);
+        protected abstract void Register(Type type);
+        protected abstract object CreateConcreteInstance(AbstractTypeConfiguration config);
+        protected abstract object[] GetConstructorParameters(AbstractTypeConfiguration config);
+    }
+}
--- Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration.ThirdParty/SimpleInjector/SimpleInjectorTask.cs
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration.ThirdParty/SimpleInjector/SimpleInjectorTask.cs
@@ -5,77 +5,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Castle.DynamicProxy;
-using Glass.Mapper.Sc.CastleWindsor.Pipelines.ObjectConstruction;
+using Glass.Mapper.Configuration;
+using Glass.Mapper.Pipelines.ObjectConstruction.Tasks.Ioc;
 using SimpleInjector;
+using LazyObjectInterceptor = Glass.Mapper.Sc.CastleWindsor.Pipelines.ObjectConstruction.LazyObjectInterceptor;
 
 namespace Glass.Mapper.Sc.Integration.ThirdParty.SimpleInjector
 {
-    public class SimpleInjectorTask : Glass.Mapper.Pipelines.ObjectConstruction.IObjectConstructionTask
+    public class SimpleInjectorTask : IocTaskBase
     {
         public static Container Container { get;  set; }
-        private static object _lock = new object();
+       
 
-         private static volatile  ProxyGenerator _generator;
-        private static volatile  ProxyGenerationOptions _options;
-
-        /// <summary>
-        /// Initializes static members of the <see cref="CreateConcreteTask"/> class.
-        /// </summary>
-        static SimpleInjectorTask()
+        protected override bool IsRegistered(Type type)
         {
-            _generator = new ProxyGenerator();
-            var hook = new Glass.Mapper.Pipelines.ObjectConstruction.Tasks.CreateConcrete.LazyObjectProxyHook();
-            _options = new ProxyGenerationOptions(hook);
+            return Container.GetRegistration(type) == null;
         }
 
-        public void Execute(Glass.Mapper.Pipelines.ObjectConstruction.ObjectConstructionArgs args)
+        protected override void Register(Type type)
         {
-            //check that no other task has created an object
-            //also check that this is a dynamic object
-            if (args.Result == null && !args.Configuration.Type.IsAssignableFrom(typeof(IDynamicMetaObjectProvider)))
-            {
-                //check to see if the type is registered with the SimpleInjector container
-                //if it isn't added it
-                if (Container.GetRegistration(args.Configuration.Type) == null)
-                {
-                    lock (_lock)
-                    {
-                        if (Container.GetRegistration(args.Configuration.Type) == null)
-                        {
-                            Container.Register(args.Configuration.Type);
-                        }
-                    }
+            Container.Register(type);
+        }
 
-                 
-                }
+        protected override object CreateConcreteInstance(AbstractTypeConfiguration config)
+        {
+            return Container.GetInstance(config.Type);
+        }
 
-                 Action<object> mappingAction = (target) => 
-                        args.Configuration.MapPropertiesToObject(target, args.Service, args.AbstractTypeCreationContext);
-
-
-                if (args.AbstractTypeCreationContext.IsLazy)
-                {
-                    var parameters = args.Configuration.ConstructorMethods.Select(x => x.Key.GetParameters())
+        protected override object[] GetConstructorParameters(AbstractTypeConfiguration config)
+        {
+            var parameters = config.ConstructorMethods.Select(x => x.Key.GetParameters())
                         .OrderBy(x => x.Length)
                         .First();
 
-                    var resolved = parameters.Select(x => Container.GetInstance(x.ParameterType));
-                    
-                    var proxy = _generator.CreateClassProxy(args.Configuration.Type, resolved.ToArray(), new LazyObjectInterceptor( mappingAction));
-                    args.Result = proxy;
-                }
-                else
-                {
-                    //create instance using SimpleInjector
-                    var obj = Container.GetInstance(args.Configuration.Type);
-
-                    //map properties from item to model
-                    mappingAction(obj);
-
-                    //set the new object as the returned result
-                    args.Result = obj;
-                }
-            }
+            var resolved = parameters.Select(x => Container.GetInstance(x.ParameterType));
+            return resolved.ToArray();
         }
     }
 }
