fe25deda3acb2af0c360ddf5d6988dd2b80f2456
Mike Edwards <mikeedwards83@googlemail.com>
2013-10-21 18:37:35 +0100

    start of code migration

---
+++ Source/Glass.Mapper.Sc.V7/ContentSearch/DocumentTypeMapper/GlassLuceneDocumentTypeMapper.cs
@@ -0,0 +1,91 @@
+﻿using Castle.DynamicProxy;
+using Glass.Mapper;
+using Glass.Mapper.Configuration;
+using Glass.Mapper.Sc;
+using Glass.Mapper.Sc.Configuration;
+using Lucene.Net.Documents;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq.Common;
+using Sitecore.ContentSearch.Linq.Methods;
+using Sitecore.ContentSearch.LuceneProvider;
+using Sitecore.ContentSearch.Security;
+using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+namespace Glass.Mapper.Sc.V7.ContentSearch.DocumentTypeMapper
+{
+   public class GlassLuceneDocumentTypeMapper : DefaultLuceneDocumentTypeMapper
+  {
+    private SitecoreContext _sitecoreContext;
+
+    public GlassLuceneDocumentTypeMapper():base()
+    {
+      
+    }
+
+    public virtual TElement MapToType<TElement>(Document document, SelectMethod selectMethod, IEnumerable<IFieldQueryTranslator> virtualFieldProcessors, SearchSecurityOptions securityOptions)
+    {
+      if (selectMethod != null)
+      {
+        ParameterInfo[] parameters = selectMethod.Delegate.Method.GetParameters();
+        if (parameters.Length != 2)
+          throw new InvalidOperationException("Invalid number of select delegate parameters.");
+        Type underlyingSystemType = parameters[1].ParameterType.UnderlyingSystemType;
+
+        var instance = Activator.CreateInstance(underlyingSystemType);
+        string[] strArray = selectMethod.FieldNames  == null || selectMethod.FieldNames.Length <= 0 ? (string[]) null : selectMethod.FieldNames;
+        ReadDocumentFields<object>(document, (IEnumerable<string>) strArray, GetTypeMap(underlyingSystemType), virtualFieldProcessors, instance);
+        return (TElement)selectMethod.Delegate.DynamicInvoke(instance);
+      }
+      else
+      {
+        DocumentTypeMapInfo typeMap = GetTypeMap<TElement>();
+        IEnumerable<string> documentFieldNames = GetDocumentFieldNames(document);
+        TElement instance = this.CreateInstance<TElement>();
+        ReadDocumentFields<TElement>(document, documentFieldNames, typeMap, virtualFieldProcessors, instance);
+        this.SetupProxy<TElement>(documentFieldNames ?? Enumerable.Select<IFieldable, string>((IEnumerable<IFieldable>) document.GetFields(), (Func<IFieldable, string>) (x => x.Name.ToLower())), (object) instance as IProxyTargetAccessor);
+        return instance;
+      }
+    }
+
+    protected virtual T CreateInstance<T>()
+    {
+      SitecoreTypeCreationContext typeCreationContext = new SitecoreTypeCreationContext();
+        typeCreationContext.RequestedType = new[] {typeof (T)};
+      typeCreationContext.IsLazy = true;
+      typeCreationContext.SitecoreService = (SitecoreService) this._sitecoreContext;
+      using (new SearchSwitcher())
+        return (T) this._sitecoreContext.InstantiateObject((AbstractTypeCreationContext) typeCreationContext);
+    }
+
+    protected void SetupProxy<T>(IEnumerable<string> fieldNames, IProxyTargetAccessor target)
+    {
+      SearchInterceptor searchInterceptor = Enumerable.FirstOrDefault<IInterceptor>((IEnumerable<IInterceptor>) target.GetInterceptors(), (Func<IInterceptor, bool>) (x => x is SearchInterceptor)) as SearchInterceptor;
+      if (searchInterceptor == null)
+        return;
+      AbstractTypeConfiguration typeConfiguration1 = this._sitecoreContext.GlassContext.GetTypeConfiguration(target);
+      SitecoreTypeConfiguration typeConfiguration2 = new SitecoreTypeConfiguration();
+      searchInterceptor.TypeConfiguration = typeConfiguration2;
+      SitecoreIdConfiguration sitecoreIdConfiguration = Enumerable.FirstOrDefault<AbstractPropertyConfiguration>(typeConfiguration1.Properties, (Func<AbstractPropertyConfiguration, bool>) (x => x is SitecoreIdConfiguration)) as SitecoreIdConfiguration;
+      if (sitecoreIdConfiguration == null)
+        return;
+      if (sitecoreIdConfiguration.PropertyInfo.PropertyType == typeof (Guid))
+      {
+        Guid id = (Guid) sitecoreIdConfiguration.PropertyInfo.GetValue((object) target);
+        searchInterceptor.Id = new ID(id);
+      }
+      else if (sitecoreIdConfiguration.PropertyInfo.PropertyType == typeof (ID))
+        searchInterceptor.Id = (ID) sitecoreIdConfiguration.PropertyInfo.GetValue((object) target);
+      foreach (AbstractPropertyConfiguration property in typeConfiguration1.Properties)
+      {
+        string propName = property.PropertyInfo.Name.ToLower();
+        if (!Enumerable.Any<string>(fieldNames, (Func<string, bool>) (x => x == propName)) && !Enumerable.Any<object>((IEnumerable<object>) property.PropertyInfo.GetCustomAttributes(true), (Func<object, bool>) (x => x is IIndexFieldNameFormatterAttribute)) && !(property is SitecoreIdConfiguration))
+          typeConfiguration2.AddProperty(property);
+      }
+      typeConfiguration2.Type = typeConfiguration1.Type;
+      typeConfiguration2.IdConfig = sitecoreIdConfiguration;
+    }
+  }
+}
---
+++ Source/Glass.Mapper.Sc.V7/ContentSearch/DocumentTypeMapper/SearchInterceptor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glass.Mapper.Sc.V7.ContentSearch.DocumentTypeMapper
+{
+    public class SearchInterceptor : IInterceptor
+    {
+        private bool _isLoaded = false;
+        private readonly ObjectConstructionArgs _args;
+        private Dictionary<string, object> _values;
+
+        public SitecoreTypeConfiguration TypeConfiguration { get; set; }
+
+        public ID Id { get; set; }
+
+        public SearchInterceptor(ObjectConstructionArgs args)
+        {
+            this._args = args;
+            this._values = new Dictionary<string, object>();
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            if (!invocation.Method.IsSpecialName || !invocation.Method.Name.StartsWith("get_") && !invocation.Method.Name.StartsWith("set_"))
+                return;
+            string str = invocation.Method.Name.Substring(0, 4);
+            string name = invocation.Method.Name.Substring(4);
+            if (!this._isLoaded && this.TypeConfiguration != null && Enumerable.Any<AbstractPropertyConfiguration>(this.TypeConfiguration.Properties, (Func<AbstractPropertyConfiguration, bool>)(x => x.PropertyInfo.Name == name)))
+            {
+                SitecoreTypeCreationContext typeCreationContext = this._args.AbstractTypeCreationContext as SitecoreTypeCreationContext;
+                typeCreationContext.Item = typeCreationContext.SitecoreService.Database.GetItem(this.Id);
+                SitecoreTypeConfiguration typeConfiguration = this.TypeConfiguration;
+                AbstractDataMappingContext dataMappingContext = this._args.Service.CreateDataMappingContext(this._args.AbstractTypeCreationContext, (object)null);
+                foreach (AbstractPropertyConfiguration propertyConfiguration in typeConfiguration.Properties)
+                {
+                    object obj = propertyConfiguration.Mapper.MapToProperty(dataMappingContext);
+                    this._values[propertyConfiguration.PropertyInfo.Name] = obj;
+                }
+                this._isLoaded = true;
+            }
+            if (str == "get_")
+            {
+                if (this._values.ContainsKey(name))
+                {
+                    object obj = this._values[name];
+                    invocation.ReturnValue = obj;
+                }
+            }
+            else if (str == "set_")
+                this._values[name] = invocation.Arguments[0];
+            else
+                throw new MapperException(Glass.Mapper.ExtensionMethods.Formatted("Method with name {0}{1} on type {2} not supported.", (object)str, (object)name, (object)this._args.Configuration.Type.FullName));
+        }
+    }
+}
