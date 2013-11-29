c2d302492c93031661bc24fe9bba7529f047290c
Michael Edwards <mikeedwards83@googlemail.com>
2013-11-29 05:43:59 +0000

    start of glimpse rework

---
+++ Source/Glass.Mapper.Glimpse.Sc/SitecoreContextTab.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glass.Mapper.Glimpse.Sc
+{
+    public class SitecoreContextTab : ContextTab
+    {
+        public override object MapTypeConfiguration(AbstractTypeConfiguration config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/Glass.Mapper.Glimpse/ContextTab.cs
@@ -0,0 +1,51 @@
+﻿using Glass.Mapper.Configuration;
+using Glimpse.Core.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glass.Mapper.Glimpse
+{
+    public abstract class ContextTab : ITab
+    {
+        public string Name
+        {
+            get { return "Glass.Mapper Context"; }
+        }
+
+        public object GetData(ITabContext context)
+        {
+            return GetContexts();
+        }
+
+        public RuntimeEvent ExecuteOn
+        {
+            get { return RuntimeEvent.EndRequest; }
+        }
+
+        public Type RequestContextType
+        {
+            get { return null; }
+        }
+
+        private object GetContexts()
+        {
+            var contextDatas = new List<object>();
+
+            foreach (var context in Glass.Mapper.Context.Contexts)
+            {
+                var contextData = new
+                    {
+                        ContextName = context.Value.Name,
+                        Configurations = context.Value.TypeConfigurations.Select(x => MapTypeConfiguration(x.Value));
+                    };
+                contextDatas.Add(contextData);
+            }
+            return contextDatas;
+        }
+
+        public abstract object MapTypeConfiguration(AbstractTypeConfiguration config);
+    }
+}
