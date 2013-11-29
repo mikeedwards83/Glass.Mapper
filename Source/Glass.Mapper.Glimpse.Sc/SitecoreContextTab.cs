7c82b48e746b24106c8eaf8c000d0dbdb2cc0dbd
Mike Edwards <mikeedwards83@googlemail.com>
2013-11-29 08:12:22 +0000

    first output of glimpse information

--- Source/Glass.Mapper.Glimpse.Sc/SitecoreContextTab.cs
+++ Source/Glass.Mapper.Glimpse.Sc/SitecoreContextTab.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Glass.Mapper.Configuration;
+using Glass.Mapper.Sc.Configuration;
+using Sitecore.Data;
 
 namespace Glass.Mapper.Glimpse.Sc
 {
@@ -10,7 +13,125 @@ namespace Glass.Mapper.Glimpse.Sc
     {
         public override object MapTypeConfiguration(AbstractTypeConfiguration config)
         {
-            throw new NotImplementedException();
+            var scConfig = config as SitecoreTypeConfiguration;
+
+            return new 
+            {
+                Type = scConfig.Type.FullName,
+                TemplateId =  ID.IsNullOrEmpty(scConfig.TemplateId) ? null : scConfig.TemplateId.ToString(),
+                TemplateName = scConfig.TemplateName,
+                BranchId = ID.IsNullOrEmpty(scConfig.BranchId) ? null : scConfig.BranchId.ToString(),
+                AutoMap = scConfig.AutoMap,
+                CodeFirst = scConfig.CodeFirst,
+                Infos = scConfig.Properties.Where(x => x is SitecoreInfoConfiguration).Select(x => Map(x as SitecoreInfoConfiguration)),
+                Fields = scConfig.Properties.Where(x => x is SitecoreFieldConfiguration).Select(x => Map(x as SitecoreFieldConfiguration)),
+                Children = scConfig.Properties.Where(x => x is SitecoreChildrenConfiguration).Select(x => Map(x as SitecoreChildrenConfiguration)),
+                Parent = scConfig.Properties.Where(x => x is SitecoreParentConfiguration).Select(x => Map(x as SitecoreParentConfiguration)),
+                Query = scConfig.Properties.Where(x => x is SitecoreQueryConfiguration).Select(x => Map(x as SitecoreQueryConfiguration)),
+                Node = scConfig.Properties.Where(x => x is SitecoreNodeConfiguration).Select(x => Map(x as SitecoreNodeConfiguration)),
+                Linked = scConfig.Properties.Where(x => x is SitecoreLinkedConfiguration).Select(x => Map(x as SitecoreLinkedConfiguration)),
+            };
+        }
+
+        protected object Map(SitecoreLinkedConfiguration config)
+        {
+            return new
+            {
+                Name = config.PropertyInfo.Name,
+                ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                config.Option,
+                config.IsLazy,
+                config.InferType
+            };
+        }
+        protected object Map(SitecoreNodeConfiguration config)
+        {
+            return new
+            {
+                Name = config.PropertyInfo.Name,
+                ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                config.Id,
+                config.Path,
+                config.IsLazy,
+                config.InferType
+            };
+        }
+        protected object Map(SitecoreQueryConfiguration config)
+        {
+            return new
+            {
+                Name = config.PropertyInfo.Name,
+                ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                config.Query,
+                config.IsRelative,
+                config.IsLazy,
+                config.InferType
+            };
+        }
+        protected object Map(SitecoreParentConfiguration config)
+        {
+            return new
+            {
+                Name = config.PropertyInfo.Name,
+                ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                config.IsLazy,
+                config.InferType
+            };
+        }
+
+        protected object Map(SitecoreChildrenConfiguration config)
+        {
+            return new
+            {
+                Name = config.PropertyInfo.Name,
+                ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                config.IsLazy,
+                config.InferType
+            };
+        }
+        protected object Map(SitecoreInfoConfiguration config)
+        {
+            return new
+            {
+                Name = config.PropertyInfo.Name,
+                ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                Type = config.Type.ToString(),
+            };
+        }
+        protected object Map(SitecoreFieldConfiguration config)
+        {
+            if (config.CodeFirst)
+            {
+                return new
+                {
+                    Name = config.PropertyInfo.Name,
+                    ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                    config.FieldName,
+                    config.FieldId,
+                    config.CodeFirst,
+                    config.FieldSortOrder,
+                    config.FieldSource,
+                    config.FieldTitle,
+                    config.FieldType,
+                    config.IsRequired,
+                    config.IsShared,
+                    config.IsUnversioned,
+                    config.SectionName,
+                    config.SectionSortOrder,
+                    config.ValidationErrorText,
+                    config.ValidationRegularExpression,
+                };
+            }
+            else
+            {
+                return new
+                {
+                    Name = config.PropertyInfo.Name,
+                    ReturnType = CleanTypeName(config.PropertyInfo.PropertyType),
+                    config.FieldName,
+                    config.FieldId
+                };
+            }
         }
     }
 }
--- Source/Glass.Mapper.Glimpse/ContextTab.cs
+++ Source/Glass.Mapper.Glimpse/ContextTab.cs
@@ -1,4 +1,5 @@
-﻿using Glass.Mapper.Configuration;
+﻿using System.Configuration;
+using Glass.Mapper.Configuration;
 using Glimpse.Core.Extensibility;
 using System;
 using System.Collections.Generic;
@@ -39,7 +40,7 @@ namespace Glass.Mapper.Glimpse
                 var contextData = new
                     {
                         ContextName = context.Value.Name,
-                        Configurations = context.Value.TypeConfigurations.Select(x => MapTypeConfiguration(x.Value));
+                        Configurations = context.Value.TypeConfigurations.Select(x => MapTypeConfiguration(x.Value))
                     };
                 contextDatas.Add(contextData);
             }
@@ -47,5 +48,35 @@ namespace Glass.Mapper.Glimpse
         }
 
         public abstract object MapTypeConfiguration(AbstractTypeConfiguration config);
+
+        protected string CleanTypeName(Type type)
+        {
+            if (type.IsGenericType)
+            {
+                var inners = type.GenericTypeArguments;
+                var sb = new StringBuilder();
+                var genericPart = type.GetGenericTypeDefinition();
+
+
+                if (genericPart == typeof (IEnumerable<>))
+                    sb.Append("IEnumerable");
+
+                sb.Append("<");
+                foreach (var inner in inners)
+                {
+                    sb.Append(CleanTypeName(inner));
+                    sb.Append(",");
+                }
+                sb.Remove(sb.Length - 1, 1);
+                sb.Append(">");
+
+                return sb.ToString();
+
+            }
+            else
+            {
+                return type.FullName;
+            }
+        }
     }
 }
