ff96cdc1d0235c40c92badaad2a19ffe2066c026
Mike Edwards <mikeedwards83@googlemail.com>
2013-12-04 11:46:47 +0000

    resharper code clean up

--- Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/Pipelines/ConfigurationResolver/TemplateInferredTypeTaskFixture.cs
+++ Tests/Integration Tests/Sitecore/Glass.Mapper.Sc.Integration/Pipelines/ConfigurationResolver/TemplateInferredTypeTaskFixture.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Glass.Mapper.Configuration.Attributes;
+﻿using Glass.Mapper.Configuration.Attributes;
 using Glass.Mapper.Pipelines.ConfigurationResolver;
 using Glass.Mapper.Sc.Configuration;
 using Glass.Mapper.Sc.Configuration.Attributes;
