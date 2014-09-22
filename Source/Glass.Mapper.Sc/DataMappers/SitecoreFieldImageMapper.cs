031be9ecc20fca2d5a214ed79da0ed4f860f3d70
Chris van de Steeg <chris@chrisvandesteeg.nl>
2014-09-22 10:16:41 +0200

    Further reverting of  commit a603537

--- Source/Glass.Mapper.Sc/DataMappers/SitecoreFieldImageMapper.cs
+++ Source/Glass.Mapper.Sc/DataMappers/SitecoreFieldImageMapper.cs
@@ -211,58 +211,6 @@ namespace Glass.Mapper.Sc.DataMappers
             MapToImage(image, imageItem);
             return image;
         }
-
-        /// <summary>
-        /// Gets a media URL.
-        /// 
-        /// </summary>
-        /// <param name="item">The media item.</param><param name="options">The query string.</param>
-        /// <returns>
-        /// The media URL.
-        /// </returns>
-        private static string GetMediaUrlWithoutContentCheck(MediaItem item, MediaUrlOptions options)
-        {
-            Assert.ArgumentNotNull((object)item, "item");
-            Assert.ArgumentNotNull((object)options, "options");
-            if (item.InnerItem["path"].Length > 0)
-            {
-                if (!options.LowercaseUrls)
-                    return item.InnerItem["path"];
-                else
-                    return item.InnerItem["path"].ToLowerInvariant();
-            }
-            else if (options.UseDefaultIcon)
-            {
-                if (!options.LowercaseUrls)
-                    return Themes.MapTheme(Settings.DefaultIcon);
-                else
-                    return Themes.MapTheme(Settings.DefaultIcon).ToLowerInvariant();
-            }
-            else
-            {
-                var mediaProvider = MediaManager.Provider;
-                Assert.IsTrue(mediaProvider.Config.MediaPrefixes[0].Length > 0, "media prefixes are not configured properly.");
-                string str1 = mediaProvider.MediaLinkPrefix;
-                if (options.AbsolutePath)
-                    str1 = options.VirtualFolder + str1;
-                else if (str1.StartsWith("/", StringComparison.InvariantCulture))
-                    str1 = StringUtil.Mid(str1, 1);
-                if (options.AlwaysIncludeServerUrl)
-                    str1 = FileUtil.MakePath(WebUtil.GetServerUrl(), str1, '/');
-                string str2 = StringUtil.EnsurePrefix('.', StringUtil.GetString(options.RequestExtension, item.Extension, "ashx"));
-                string str3 = options.ToString();
-                if (str3.Length > 0)
-                    str2 = str2 + "?" + str3;
-                string str4 = "/sitecore/media library/";
-                string path = item.InnerItem.Paths.Path;
-                string str5 = !options.UseItemPath || !path.StartsWith(str4, StringComparison.OrdinalIgnoreCase) ? item.ID.ToShortID().ToString() : StringUtil.Mid(path, str4.Length);
-                string str6 = str1 + str5 + (options.IncludeExtension ? str2 : string.Empty);
-                if (!options.LowercaseUrls)
-                    return str6;
-                else
-                    return str6.ToLowerInvariant();
-            }
-        }
     }
 }
 
