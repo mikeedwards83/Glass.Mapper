80c86d45e7097b6694b4cf14785b87777ed2136e
Mike Edwards <mikeedwards83@googlemail.com>
2014-11-21 15:52:43 +0000

    fixes for sitecore initialise pipeline

--- Source/Glass.Mapper.Sc.CastleWindsor/App_Start/GlassMapperSc.cs.pp
+++ Source/Glass.Mapper.Sc.CastleWindsor/App_Start/GlassMapperSc.cs.pp
@@ -8,6 +8,7 @@ using System;
 using System.Linq;
 using Glass.Mapper.Sc.CastleWindsor;
 using Glass.Mapper.Sc.Configuration.Attributes;
+using Sitecore.Pipelines;
 
 // WebActivator has been removed. If you wish to continue using WebActivator uncomment the line below
 // and delete the Glass.Mapper.Sc.CastleWindsor.config file from the Sitecore Config Include folder.
@@ -15,8 +16,12 @@ using Glass.Mapper.Sc.Configuration.Attributes;
 
 namespace $rootnamespace$.App_Start
 {
-	public static class  GlassMapperSc
+	public class  GlassMapperSc
 	{
+		public void Process(PipelineArgs args){
+			GlassMapperSc.Start();
+		}
+
 		public static void Start()
 		{
 			//create the resolver
--- Source/Glass.Mapper.Sc.Razor.CastleWindsor/App_Start/GlassMapperScRazor.cs.pp
+++ Source/Glass.Mapper.Sc.Razor.CastleWindsor/App_Start/GlassMapperScRazor.cs.pp
@@ -10,6 +10,7 @@ using Glass.Mapper.Sc.CastleWindsor;
 using Glass.Mapper.Sc.Configuration.Attributes;
 using Glass.Mapper.Sc.Razor;
 using Glass.Mapper.Configuration.Attributes;
+using Sitecore.Pipelines;
 
 // WebActivator has been removed. If you wish to continue using WebActivator uncomment the line below
 // and delete the Glass.Mapper.Sc.CastleWindsor.config file from the Sitecore Config Include folder.
@@ -17,8 +18,12 @@ using Glass.Mapper.Configuration.Attributes;
 
 namespace $rootnamespace$.App_Start
 {
-	public static class  GlassMapperScRazor
+	public  class  GlassMapperScRazor
 	{
+	    public void Process(PipelineArgs args){
+			GlassMapperScRazor.Start();
+		}
+
 		public static void Start()
 		{
 			//create config
