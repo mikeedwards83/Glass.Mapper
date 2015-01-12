8779ed5f4e38625ddc78552c7049b48535a7b13c
Michael Edwards <mikeedwards83@googlemail.com>
2015-01-12 21:03:18 +0000

    start of bug fix for 99

---
+++ Source/Glass.Mapper/UrlBuilder.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glass.Mapper
+{
+    public class UrlBuilder
+    {
+        private readonly string _url;
+        public List<KeyValuePair<string, string>> QueryString { get; private set; }
+
+        public UrlBuilder(string url)
+        {
+            _url = url;
+            QueryString = new List<KeyValuePair<string, string>>();
+            ProcessQueryString(_url, QueryString);
+        }
+
+        protected void ProcessQueryString(string url, List<KeyValuePair<string, string>> queryString)
+        {
+            if (url.IsNotNullOrEmpty() && url.IndexOf("?", StringComparison.Ordinal) > -1)
+            {
+                var urlQuery = url.Split('?');
+                var query = urlQuery[1];
+
+                if (query.IsNotNullOrEmpty())
+                {
+                    query = query.Replace("&amp;", "&");
+                    var pairs = query.Split('&', StringSplitOptions.RemoveEmptyEntries);
+                    foreach (var pair in pairs)
+                    {
+                        var keyValue = pair.Split('=');
+                        if (keyValue.Length == 2)
+                        {
+                            QueryString.Add(new KeyValuePair<string, string>(keyValue[0] ?? string.Empty,
+                                keyValue[1] ?? string.Empty));
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="queryString">A query string containing multiple pairs, e.g. ?key1=value1&key2=value2</param>
+        public virtual void AddToQueryString(string queryString)
+        {
+            if (queryString.IndexOf("?") < 0)
+            {
+                queryString = "?" + queryString;
+            }
+            ProcessQueryString(queryString, QueryString);
+        }
+
+        public virtual void AddToQueryString(string key, string value)
+        {
+            if (key.IsNotNullOrEmpty() && value.IsNotNullOrEmpty())
+            {
+                QueryString.Add(new KeyValuePair<string, string>(key, value));
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (_url.IsNotNullOrEmpty())
+            {
+                var urlFirst = _url.Split('?')[0];
+                sb.Append(urlFirst);
+            }
+            if (QueryString.Count > 0)
+            {
+                sb.Append("?");
+            }
+            foreach (var keyValuePair in QueryString)
+            {
+                if (keyValuePair.Key.IsNotNullOrEmpty())
+                {
+                    sb.AppendFormat("{0}={1}&", keyValuePair.Key ?? string.Empty, keyValuePair.Value ?? string.Empty);
+                }
+            }
+
+            if (QueryString.Count > 0)
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests/Unit Tests/Glass.Mapper.Tests/UrlBuilderFixture.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Glass.Mapper.Tests
+{
+    [TestFixture]
+    public class UrlBuilderFixture
+    {
+        [Test]
+        [TestCase("http://glass.co/test/somevalue", "http://glass.co/test/somevalue")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=another", "http://glass.co/test/somevalue?test=value&test1=another")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=another&test=value3", "http://glass.co/test/somevalue?test=value&test1=another&test=value3")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=another&test=value3&", "http://glass.co/test/somevalue?test=value&test1=another&test=value3")]
+        [TestCase(null, "")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=", "http://glass.co/test/somevalue?test=value&test1=")]
+        [TestCase("http://glass.co/test/somevalue?test=value&=another&test=value3", "http://glass.co/test/somevalue?test=value&test=value3")]
+        public void UrlBuilder_TestUrlShouldRemainTheSame_NoAdditionalQueryParameters(string url, string expected)
+        {
+            //Arrange
+            
+            //Act
+            var builder = new UrlBuilder(url);
+
+            //Assert
+            Assert.AreEqual(expected, builder.ToString());
+        }
+
+        [Test]
+        [TestCase("http://glass.co/test/somevalue", "http://glass.co/test/somevalue?k1=v1&v2=k2", "k1=v1&v2=k2")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=another", "http://glass.co/test/somevalue?test=value&test1=another&k1=v1&v2=k2", "?k1=v1&v2=k2")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=another&test=value3", "http://glass.co/test/somevalue?test=value&test1=another&test=value3&k1=v1&v2=k2", "?k1=v1&v2=k2")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=another&test=value3&", "http://glass.co/test/somevalue?test=value&test1=another&test=value3&k1=v1&v2=k2", "?k1=v1&v2=k2")]
+        [TestCase(null, "?k1=v1&v2=k2", "?k1=v1&v2=k2")]
+        [TestCase("http://glass.co/test/somevalue?test=value&test1=", "http://glass.co/test/somevalue?test=value&test1=&k1=v1&v2=k2", "?k1=v1&v2=k2")]
+        [TestCase("http://glass.co/test/somevalue?test=value&=another&test=value3", "http://glass.co/test/somevalue?test=value&test=value3&k1=v1&v2=k2", "?k1=v1&v2=k2")]
+        public void AddToQueryString_AddsCompleteString_ReturnsValueUrl(string url, string expected, string query)
+        {
+            //Arrange
+            var builder = new UrlBuilder(url);
+
+            //Act
+            builder.AddToQueryString(query);
+
+            //Assert
+            Assert.AreEqual(expected, builder.ToString());
+        }
+
+
+    }
+}
