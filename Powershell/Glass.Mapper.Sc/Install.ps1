70fa7923dc11fda3a3e82f596ef8ee52eb2afd66
Mike Edwards <mikeedwards83@googlemail.com>
2015-04-14 10:19:08 +0100

    Glass.Mapper.Sc powershell script 1

---
+++ Powershell/Glass.Mapper.Sc/Install.ps1
@@ -0,0 +1,24 @@
+param($installPath, $toolsPath, $package, $project)
+
+$scKernel = $project.Object.References.Item('Sitecore.Kernel').FullPath
+
+$scFileVersion = (Get-item $scKernel).VersionInfo.FileVersion;
+
+$scVerion = [double]::Parse([string]::Format("{0}.{1}",$scFileVersion.Split(".")[0], $scFileVersion.Split(".")[1]));
+$gmsPath = String.Empty;
+
+if($scVersion -ge 7.5){
+	$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+}
+else{ 
+	$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+}
+
+
+$existing = $project.Object.References.Item("Glass.Mapper.Sc");
+if(existing -ne null){
+	$existing.Remove();
+}
+
+$project.Object.References.Add($gmsPath);
+$project.Object.References.Item("Glass.Mapper.Sc").CopyLocal = "True"
