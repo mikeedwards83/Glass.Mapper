1b96a230c794342ab3ca6ce1c15284acbd6abc1d
Mike Edwards <mikeedwards83@googlemail.com>
2015-04-14 10:40:06 +0100

    moving Install.ps1 to tools folder and adding more output

--- Powershell/Glass.Mapper.Sc/Install.ps1
+++ Powershell/Glass.Mapper.Sc/Install.ps1
@@ -7,6 +7,9 @@ $scKernel = $project.Object.References.Item('Sitecore.Kernel').FullPath
 
 $scFileVersion = (Get-item $scKernel).VersionInfo.FileVersion;
 
+Write-Host "Check Sitecore File Version is "+$scFileVersion;
+
+
 $scVerion = [double]::Parse([string]::Format("{0}.{1}",$scFileVersion.Split(".")[0], $scFileVersion.Split(".")[1]));
 $gmsPath = String.Empty;
 
@@ -17,11 +20,13 @@ else{
 	$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
 }
 
-
 $existing = $project.Object.References.Item("Glass.Mapper.Sc");
 if(existing -ne null){
+    Write-Host "Removing existing Glass.Mapper.Sc reference";
 	$existing.Remove();
 }
+
+Write-Host "Adding reference to "+$gmsPath;
 
 $project.Object.References.Add($gmsPath);
 $project.Object.References.Item("Glass.Mapper.Sc").CopyLocal = "True"
