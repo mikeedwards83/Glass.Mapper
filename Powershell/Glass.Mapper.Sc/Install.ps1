ea05e1d1632336f012294ad5fce982c59b413d6b
Mike Edwards <mikeedwards83@googlemail.com>
2015-04-14 10:49:18 +0100

    more error checking

--- Powershell/Glass.Mapper.Sc/Install.ps1
+++ Powershell/Glass.Mapper.Sc/Install.ps1
@@ -3,30 +3,39 @@ param($installPath, $toolsPath, $package, $project)
 
 Write-Host "Check Sitecore Version"
 
-$scKernel = $project.Object.References.Item('Sitecore.Kernel').FullPath
+$scKernel = $project.Object.References.Item('Sitecore.Kernel');
 
-$scFileVersion = (Get-item $scKernel).VersionInfo.FileVersion;
-
-Write-Host "Check Sitecore File Version is "+$scFileVersion;
-
-
-$scVerion = [double]::Parse([string]::Format("{0}.{1}",$scFileVersion.Split(".")[0], $scFileVersion.Split(".")[1]));
-$gmsPath = String.Empty;
-
-if($scVersion -ge 7.5){
-	$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+if($scKernel -eq null){
+	Write-Host "Could not locate Sitecore.Kernel.dll, please add reference before installing Glass.Mapper.Sc";
 }
-else{ 
-	$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+else{
+	$scKernelPath = $scKernel.Path
+
+	Write-Host "Sitecore.Kernel Path: "+$scKernelPath;
+
+	$scFileVersion = (Get-item $scKernel).VersionInfo.FileVersion;
+
+	Write-Host "Check Sitecore File Version is "$scFileVersion;
+
+
+	$scVerion = [double]::Parse([string]::Format("{0}.{1}",$scFileVersion.Split(".")[0], $scFileVersion.Split(".")[1]));
+	$gmsPath = "";
+
+	if($scVersion -ge 7.5){
+		$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+	}
+	else{ 
+		$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+	}
+
+	$existing = $project.Object.References.Item("Glass.Mapper.Sc");
+	if(existing -ne null){
+		Write-Host "Removing existing Glass.Mapper.Sc reference";
+		$existing.Remove();
+	}
+
+	Write-Host "Adding reference to "$gmsPath;
+
+	$project.Object.References.Add($gmsPath);
+	$project.Object.References.Item("Glass.Mapper.Sc").CopyLocal = "True"
 }
-
-$existing = $project.Object.References.Item("Glass.Mapper.Sc");
-if(existing -ne null){
-    Write-Host "Removing existing Glass.Mapper.Sc reference";
-	$existing.Remove();
-}
-
-Write-Host "Adding reference to "+$gmsPath;
-
-$project.Object.References.Add($gmsPath);
-$project.Object.References.Item("Glass.Mapper.Sc").CopyLocal = "True"
