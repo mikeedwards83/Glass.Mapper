9e25571a274fc657f1c94b24d0c49e8da4c1288b
Mike Edwards <mikeedwards83@googlemail.com>
2015-04-14 11:32:51 +0100

    updating script to use functions and correct dll directory

--- Powershell/Glass.Mapper.Sc/Install.ps1
+++ Powershell/Glass.Mapper.Sc/Install.ps1
@@ -1,9 +1,11 @@
 param($installPath, $toolsPath, $package, $project)
 
 
+
+
 Write-Host "Check Sitecore Version"
 
-$scKernel = $project.Object.References.Item('Sitecore.Kernel');
+$scKernel = $project.Object.References.Item("Sitecore.Kernel");
 
 if($scKernel){	
 	$scKernelPath = $scKernel.Path
@@ -18,24 +20,54 @@ if($scKernel){
 	$scVerion = [double]::Parse([string]::Format("{0}.{1}",$scFileVersion.Split(".")[0], $scFileVersion.Split(".")[1]));
 	$gmsPath = "";
 
-	if($scVersion -ge 7.5){
-		$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+	if($scVersion -ge 8.0){
+		$gmsPath = $installPath +"\libs\80\"
+	}
+	elseif($scVersion -ge 7.5){
+		$gmsPath = $installPath +"\libs\75\"
+	}
+	elseif($scVersion -ge 7.2){
+		$gmsPath = $installPath +"\libs\72\"
+	}
+	elseif($scVersion -ge 7.1){
+		$gmsPath = $installPath +"\libs\71\"
 	}
 	else{ 
-		$gmsPath = $installPath +"\libs\sc75\Glass.Mapper.Sc.dll"
+		$gmsPath = $installPath +"\libs\70\"
 	}
 
-	$existing = $project.Object.References.Item("Glass.Mapper.Sc");
-	if($existing){
-		Write-Host "Removing existing Glass.Mapper.Sc reference";
-		$existing.Remove();
-	}
 
-	Write-Host "Adding reference to "$gmsPath;
+	Function RemovingExisting{
+		param($name);
 
-	$project.Object.References.Add($gmsPath);
-	$project.Object.References.Item("Glass.Mapper.Sc").CopyLocal = "True"
+		$existing =  $project.Object.References.Item($name);
+
+		if($existing){
+			Write-Host "Removing existing "$name;
+			$existing.Remove();
+		}
+    }
+
+	Function AddReference{
+		param($name);
+
+
+		$finalPath= "{0}{1}" -f $gmsPath,$name;
+
+		Write-Host "Adding reference to "$name;
+
+		$project.Object.References.Add($finalPath);
+		$project.Object.References.Item("Glass.Mapper.Sc").CopyLocal = "True"
+    }
+
+
+
+	RemovingExisting("Glass.Mapper.Sc");
+	AddReference("Glass.Mapper.Sc");
+	
 }
 else{
 	Write-Host "Could not locate Sitecore.Kernel.dll, please add reference before installing Glass.Mapper.Sc";
 }
+
+
