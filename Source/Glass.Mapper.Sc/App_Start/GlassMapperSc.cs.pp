371793c9db690c72d5edc1a7332c8c600d2427ed
Mike Edwards <mikeedwards83@googlemail.com>
2015-04-16 12:14:54 +0100

    updated the PP files for IOC removal

---
+++ Source/Glass.Mapper.Sc/App_Start/GlassMapperSc.cs.pp
@@ -0,0 +1,37 @@
+﻿/*************************************
+
+DO NOT CHANGE THIS FILE - UPDATE GlassMapperScCustom.cs
+
+**************************************/
+
+using Glass.Mapper.Sc.IoC;
+using Sitecore.Pipelines;
+
+// WebActivator has been removed. If you wish to continue using WebActivator uncomment the line below
+// and delete the Glass.Mapper.Sc.CastleWindsor.config file from the Sitecore Config Include folder.
+// [assembly: WebActivatorEx.PostApplicationStartMethod(typeof($rootnamespace$.App_Start.GlassMapperSc), "Start")]
+
+namespace $rootnamespace$.App_Start
+{
+	public class  GlassMapperSc
+	{
+		public void Process(PipelineArgs args){
+			GlassMapperSc.Start();
+		}
+
+		public static void Start()
+		{
+			//install the custom services
+			var resolver = GlassMapperScCustom.CreateResolver();
+
+			//create a context
+			var context = Glass.Mapper.Context.Create(resolver);
+
+			context.Load(      
+				GlassMapperScCustom.GlassLoaders()        				
+				);
+
+			GlassMapperScCustom.PostLoad();
+		}
+	}
+}
