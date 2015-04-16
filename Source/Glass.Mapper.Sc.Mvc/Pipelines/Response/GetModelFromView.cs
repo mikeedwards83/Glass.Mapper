fba46ed961007250657e8eff15402e8fd25679ab
Mike Edwards <mikeedwards83@googlemail.com>
2015-04-16 16:09:47 +0100

    Added GetModelFromView to Sitecore MVC solution

---
+++ Source/Glass.Mapper.Sc.Mvc/Pipelines/Response/GetModelFromView.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Compilation;
+using Glass.Mapper.Sc.ModelCache;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.Mvc.Pipelines.Response.GetModel;
+using Sitecore.Data.Items;
+
+namespace Glass.Mapper.Sc.Pipelines.Response
+{
+    public class GetModelFromView : GetModelProcessor
+    {
+        private readonly IModelCacheManager modelCacheManager;
+
+        public GetModelFromView()
+            : this(new ModelCacheManager())
+        {
+
+        }
+
+        public GetModelFromView(IModelCacheManager modelCacheManager)
+        {
+            this.modelCacheManager = modelCacheManager;
+        }
+
+        public override void Process(GetModelArgs args)
+        {
+            if (!IsValidForProcessing(args))
+            {
+                return;
+            }
+
+            string path = GetViewPath(args);
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return;
+            }
+
+            string cacheKey = modelCacheManager.GetKey(path);
+            Type modelType = modelCacheManager.Get(cacheKey);
+
+            if (modelType == typeof(NullModel))
+            {
+                // The model has been attempted before and is not useful
+                return;
+            }
+
+            // The model type hasn't been found before or has been cleared.
+            if (modelType == null)
+            {
+                modelType = GetModel(args, path);
+
+                modelCacheManager.Add(cacheKey, modelType);
+
+                if (modelType == typeof(NullModel))
+                {
+                    // This is not the type we are looking for
+                    return;
+                }
+            }
+
+            // If you are using an orm, replace this line with an orm specific implementation.
+            args.Result = Activator.CreateInstance(modelType);
+            Assert.IsNotNull(args.Result, "args.Result");
+        }
+
+        private string GetPathFromLayout(
+            Database db,
+            ID layoutId)
+        {
+            Item layout = db.GetItem(layoutId);
+
+            return layout != null
+                ? layout["path"]
+                : null;
+        }
+
+        private string GetViewPath(GetModelArgs args)
+        {
+            string path = args.Rendering.RenderingItem.InnerItem["path"];
+
+            if (string.IsNullOrWhiteSpace(path) && args.Rendering.RenderingType == "Layout")
+            {
+                path = GetPathFromLayout(args.PageContext.Database, new ID(args.Rendering.LayoutId));
+            }
+            return path;
+        }
+
+        private Type GetModel(GetModelArgs args, string path)
+        {
+            Type compiledViewType = BuildManager.GetCompiledType(path);
+            Type baseType = compiledViewType.BaseType;
+
+            if (baseType == null || !baseType.IsGenericType)
+            {
+                Log.Error(string.Format(
+                    "View {0} compiled type {1} base type {2} does not have a single generic argument.",
+                    args.Rendering.RenderingItem.InnerItem["path"],
+                    compiledViewType,
+                    baseType), this);
+                return typeof(NullModel);
+            }
+
+            Type proposedType = baseType.GetGenericArguments()[0];
+            return proposedType == typeof(object)
+                ? typeof(NullModel)
+                : proposedType;
+        }
+
+        private static bool IsValidForProcessing(GetModelArgs args)
+        {
+            if (args.Result != null)
+            {
+                return false;
+            }
+
+            if (!String.IsNullOrEmpty(args.Rendering.RenderingItem.InnerItem["Model"]))
+            {
+                return false;
+            }
+
+            return args.Rendering.RenderingType == "Layout" ||
+                   args.Rendering.RenderingType == "View" ||
+                   args.Rendering.RenderingType == "r";
+        }
+    }
+
+    public class NullModel
+    {
+
+    }
+}
