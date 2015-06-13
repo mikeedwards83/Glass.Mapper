5dc69cef92e6cace833e0bdd79b822e04f487b6c
Michael Edwards <mikeedwards83@googlemail.com>
2015-06-13 20:18:20 +0100

    added property and activator speed tests

---
+++ Tests/Glass.Sandbox/InstantiationSpeedTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Linq.Expressions;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Security;
+
+namespace Glass.Sandbox
+{
+    [TestFixture]
+    public class InstantiationSpeedTests
+    {
+        [Test]
+        public void CreateObject()
+        {
+            //Arrange
+            var stopWatch1 = new Stopwatch();
+            var stopWatch2 = new Stopwatch();
+            var stopWatch3 = new Stopwatch();
+            var stopWatch4 = new Stopwatch();
+            var stopWatch5 = new Stopwatch();
+            var stopWatch6 = new Stopwatch();
+
+            var type = typeof (Stub);
+            var lamda = LamdaMethod(typeof(Stub));
+            var jitted = LamdaMethod(typeof (Stub));
+            var constructor = type.GetConstructors()[0];
+
+            var il = IlMethod(typeof(Stub));
+
+            //Act
+
+            stopWatch1.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                var obj1 = new Stub();
+            }
+            stopWatch1.Stop();
+
+            stopWatch2.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                var obj2 =Activator.CreateInstance<Stub>();
+            }
+            stopWatch2.Stop();
+
+            stopWatch3.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                var obj3 = lamda();
+            }
+            stopWatch3.Stop();
+
+            stopWatch4.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                var obj4 = il.Invoke(null,null);
+            }
+            stopWatch4.Stop();
+
+            stopWatch5.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                var obj5 = jitted();
+            }
+            stopWatch5.Stop();
+
+
+            stopWatch6.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                var obj6 = FormatterServices.GetUninitializedObject(type);
+                constructor.Invoke(obj6, null);
+            }
+            stopWatch6.Stop();
+
+
+            //Assert
+            Console.WriteLine("Raw: {0} Activator: {1} Lambda: {2} IL: {3} Jit: {4} Constructor: {5}", stopWatch1.ElapsedTicks, stopWatch2.ElapsedTicks, stopWatch3.ElapsedTicks, stopWatch4.ElapsedTicks, stopWatch5.ElapsedTicks, stopWatch6.ElapsedTicks);
+
+        }
+
+
+
+        private DynamicMethod IlMethod(Type type)
+        {
+            var method = new DynamicMethod("", typeof (object), Type.EmptyTypes);
+            var il = method.GetILGenerator();
+
+
+            var ctor = type.GetConstructor(Type.EmptyTypes);
+            il.Emit(OpCodes.Newobj, ctor);
+            il.Emit(OpCodes.Ret);
+
+            return method;
+        }
+
+        static Func<object> LamdaMethod(Type type)
+        {
+            return Expression.Lambda<Func<object>>(
+                Expression.Convert(Expression.New(type), typeof(object)))
+                .Compile();
+        }
+        public class Stub { }
+
+        public Func<object> Jitted(Type type)
+        {
+            Func<object> func = Expression.Lambda<Func<object>>(
+                Expression.Convert(Expression.New(type), typeof (object))).Compile();
+
+            JitCompile(func);
+            return func;
+        }
+
+        [SecuritySafeCritical]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        internal static void JitCompile(Delegate del)
+        {
+            RuntimeHelpers.PrepareDelegate(del);
+        }
+    }
+}
---
+++ Tests/Glass.Sandbox/PropertySpeedTest.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Glass.Sandbox
+{
+    [TestFixture]
+    public class PropertySpeedTest
+    {
+        [Test]
+        public void SetProperty()
+        {
+            //Arrange
+            var type = typeof (Stub);
+            var property = type.GetProperty("Value");
+            var stopWatch1 = new Stopwatch();
+            var stopWatch2 = new Stopwatch();
+            var stopWatch3 = new Stopwatch();
+            var stopWatch4 = new Stopwatch();
+            var lambda = Lambda(property);
+            var value = "test";
+            var il = CreateSetMethod(property);
+
+            //Act
+
+            var obj1 = new Stub();
+            stopWatch1.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                obj1.Value = value;
+            }
+            stopWatch1.Stop();
+
+            var obj2 = new Stub();
+            stopWatch2.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                lambda(obj2, value);
+            }
+            stopWatch2.Stop();
+
+            var obj3 = new Stub();
+            stopWatch3.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                il(obj3, value);
+            }
+            stopWatch3.Stop();
+
+
+            var obj4 = new Stub();
+            stopWatch4.Start();
+            for (int i = 0; i < 50000; i++)
+            {
+                property.SetMethod.Invoke(obj4, new []{value});
+            }
+            stopWatch4.Stop();
+
+            //Assert
+            
+            Console.WriteLine("Standard {0} lambda {1} il {2} setmethod {3}", stopWatch1.ElapsedTicks, stopWatch2.ElapsedTicks, stopWatch3.ElapsedTicks, stopWatch4.ElapsedTicks);
+
+
+
+        }
+
+        public delegate void GenericSetter(object target, object value);
+
+        private static GenericSetter CreateSetMethod(PropertyInfo propertyInfo)
+        {
+            /*
+            * If there's no setter return null
+            */
+            MethodInfo setMethod = propertyInfo.GetSetMethod();
+            if (setMethod == null)
+                return null;
+
+            /*
+            * Create the dynamic method
+            */
+            Type[] arguments = new Type[2];
+            arguments[0] = arguments[1] = typeof(object);
+
+            DynamicMethod setter = new DynamicMethod(
+              String.Concat("_Set", propertyInfo.Name, "_"),
+              typeof(void), arguments, propertyInfo.DeclaringType);
+            ILGenerator generator = setter.GetILGenerator();
+            generator.Emit(OpCodes.Ldarg_0);
+            generator.Emit(OpCodes.Castclass, propertyInfo.DeclaringType);
+            generator.Emit(OpCodes.Ldarg_1);
+
+            if (propertyInfo.PropertyType.IsClass)
+                generator.Emit(OpCodes.Castclass, propertyInfo.PropertyType);
+            else
+                generator.Emit(OpCodes.Unbox_Any, propertyInfo.PropertyType);
+
+            generator.EmitCall(OpCodes.Callvirt, setMethod, null);
+            generator.Emit(OpCodes.Ret);
+
+            /*
+            * Create the delegate and return it
+            */
+            return (GenericSetter)setter.CreateDelegate(typeof(GenericSetter));
+        }
+
+
+        private static Action<object, object> Lambda(PropertyInfo propertyInfo)
+        {
+            Type propertyType = propertyInfo.PropertyType;
+            Type type = propertyInfo.DeclaringType;
+
+            ParameterExpression instanceParameter = Expression.Parameter(typeof(object), "instance");
+            ParameterExpression valueParameter = Expression.Parameter(typeof(object), "value");
+
+            Expression<Action<object, object>> lambda = Expression.Lambda<Action<object, object>>(
+                Expression.Assign(
+                    Expression.Property(Expression.Convert(instanceParameter, type), propertyInfo),
+                    Expression.Convert(valueParameter, propertyType)),
+                instanceParameter,
+                valueParameter
+                );
+
+            return lambda.Compile();
+        }
+
+
+        public class Stub
+        {
+            public string Value { get; set; }
+
+        }
+
+    }
+}
