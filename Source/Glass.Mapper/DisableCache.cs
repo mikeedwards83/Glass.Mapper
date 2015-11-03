d727bd6361ba0813a8100d480335c639a9bf2559
Mike Edwards <mike@glass.lu>
2015-11-03 08:26:34 +0000

    Reworked DisableCache to use stack

--- Source/Glass.Mapper/DisableCache.cs
+++ Source/Glass.Mapper/DisableCache.cs
@@ -2,64 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 
 namespace Glass.Mapper
 {
-    public class DisableCache : IDisposable
+    public class DisableCache :SettingStack<CacheSetting>
     {
-        private static CacheSetting _cacheSetting;
-        private  const string ItemsKey = "D4F757BB-B53C-4600-8264-AA94D7D5050A";
-        private bool _isDisposed;
-
-        public DisableCache()
+        public DisableCache() : base(CacheSetting.Disabled)
         {
-            Current = CacheSetting.Disabled;
         }
-
-        public static CacheSetting Current
-        {
-            get
-            {
-                if (HttpContext.Current == null)
-                {
-                    return _cacheSetting;
-                    
-                }
-                if (HttpContext.Current.Items[ItemsKey] != null)
-                {
-                    return (CacheSetting) HttpContext.Current.Items[ItemsKey];
-                }
-                return CacheSetting.Enabled;
-                
-            }
-            set
-            {
-                if (HttpContext.Current == null)
-                {
-                    _cacheSetting = value;
-
-                }
-                else
-                {
-                    HttpContext.Current.Items[ItemsKey] = value;
-                }
-            }
-        }
-
-
-        public void Dispose()
-        {
-            if (_isDisposed)
-            {
-                return;
-            }
-
-            _isDisposed = true;
-            Current = CacheSetting.Enabled;
-        }
-
     }
 
     public enum CacheSetting
