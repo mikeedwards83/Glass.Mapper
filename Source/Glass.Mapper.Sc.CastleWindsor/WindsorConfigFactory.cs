e29fd7f3e9ae08a907e751ab56f836d5a904aa0f
Mike Edwards <mike@glass.lu>
2016-05-05 10:22:17 +0100

    Added to IOC InsertBefore and InsertAfter

--- Source/Glass.Mapper.Sc.CastleWindsor/WindsorConfigFactory.cs
+++ Source/Glass.Mapper.Sc.CastleWindsor/WindsorConfigFactory.cs
@@ -14,26 +14,36 @@ namespace Glass.Mapper.Sc.CastleWindsor
             this.container = container;
         }
 
-        public void Insert(int index, Func<T> add)
+        public void Insert<TK>(int index, Func<TK> add) where TK : T
         {
             throw new NotSupportedException("Configuration cannot be added to container based config factories");
         }
 
-        public void Add(Func<T> add)
+        public void Add<TK>(Func<TK> add) where TK : T
         {
             throw new NotSupportedException("Configuration cannot be added to container based config factories");
         }
 
-        public void First(Func<T> add)
+        public void First<TK>(Func<TK> add) where TK : T
         {
             throw new NotSupportedException("Configuration cannot be added to container based config factories");
 
         }
 
-        public void Replace(int index, Func<T> replace)
+        public void Replace<TK>(int index, Func<TK> replace) where TK:T
         {
             throw new NotSupportedException("Configuration cannot be added to container based config factories");
 
+        }
+
+        public void InsertBefore<TBefore, TK>(Func<TK> func) where TBefore : T where TK : T
+        {
+            throw new NotSupportedException("Configuration cannot be added to container based config factories");
+        }
+
+        public void InsertAfter<TAfter, TK>(Func<TK> func) where TAfter : T where TK : T
+        {
+            throw new NotSupportedException("Configuration cannot be added to container based config factories");
         }
 
         public IEnumerable<T> GetItems()
--- Source/Glass.Mapper/IoC/IConfigFactory.cs
+++ Source/Glass.Mapper/IoC/IConfigFactory.cs
@@ -5,13 +5,17 @@ namespace Glass.Mapper.IoC
 {
     public interface IConfigFactory<T>
     {
-        void Insert(int index, Func<T> add);
+        void Insert<TK>(int index, Func<TK> add) where TK : T;
 
-        void Add(Func<T> add);
+        void Add<TK>(Func<TK> add) where TK : T;
 
-        void First(Func<T> add);
+        void First<TK>(Func<TK> add) where TK : T;
 
-        void Replace(int index, Func<T> replace);
+        void Replace<TK>(int index, Func<TK> replace) where TK : T;
+
+        void InsertBefore<TBefore, TK>(Func<TK> func) where TBefore : T where TK : T;
+
+        void InsertAfter<TAfter, TK>(Func<TK> func) where TAfter : T where TK : T;
 
         IEnumerable<T> GetItems();
 
