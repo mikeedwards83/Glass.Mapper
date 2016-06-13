2f84ea7121258c41408ec5eb63fa5d593e91142d
Nathanael Mann <nathanael.mann@cit-design.co.uk>
2016-06-13 13:03:43 +0100

    Added the object fix.

--- Source/Glass.Mapper/Context.cs
+++ Source/Glass.Mapper/Context.cs
@@ -24,7 +24,6 @@ using Glass.Mapper.Configuration;
 using Glass.Mapper.Pipelines.ConfigurationResolver.Tasks.OnDemandResolver;
 using Glass.Mapper.Pipelines.DataMapperResolver;
 using System.Collections.Concurrent;
-using Castle.Core.Logging;
 using Glass.Mapper.IoC;
 
 namespace Glass.Mapper
@@ -168,9 +167,9 @@ namespace Glass.Mapper
                 foreach (var typeConfig in typeConfigurations)
                 {
 
-                    //don't load generic types
+                    //don't load generic types or specifically the object type
                     //see https://github.com/mikeedwards83/Glass.Mapper/issues/85
-                    if (typeConfig.Type.IsGenericTypeDefinition)
+                    if (typeConfig.Type.IsGenericTypeDefinition || typeConfig.Type == typeof(object))
                     {
                         continue;
                     }
