dea994f24264a8b5bf78fec7f96cfbd5a092217e
Mike Edwards <mike@glass.lu>
2016-12-18 10:01:50 +0000

    Updated EditFrameBuilder prefix so that it can be user set

--- Source/Glass.Mapper.Sc/Pipelines/GetChromeData/EditFrameBuilder.cs
+++ Source/Glass.Mapper.Sc/Pipelines/GetChromeData/EditFrameBuilder.cs
@@ -18,17 +18,20 @@ namespace Glass.Mapper.Sc.Pipelines.GetChromeData
         private readonly TemplateID _folderTemplateId = new TemplateID(new ID("{B0666CFE-8C7B-4CC1-8E32-8253742EBFA9}"));
         private readonly TemplateID _editTemplateId = new TemplateID(new ID("{1AB4F9AD-B004-413C-8924-3E07143A614B}"));
 
-        public const string EditFrameItemPrefix = "Glass-";
-        private const string EditName = "Edit Fields";
-        private const string HeaderField = "Header";
-        private const string FieldsField = "Fields";
-        private const string DefaultTitle = "Edit Fields";
-        private const string DefaultTitleField = "Default Title";
-        private const string DefaultTooltipField = "Default Tooltip";
-        private const string TooltipField = "Tooltip";
-        private const string IconField = "Icon";
-        private const string Tooltip = "Edit the following fields: {0}";
-        private const string Icon = "people/16x16/cubes_blue.png";
+        /// <summary>
+        /// Prefixes for items created in the core database when using dynamic edit frames. Default value is empty.
+        /// </summary>
+        public static string EditFrameItemPrefix = "";
+        public static string EditName = "Edit Fields";
+        private static string HeaderField = "Header";
+        private static string FieldsField = "Fields";
+        public static string DefaultTitle = "Edit Fields";
+        private static string DefaultTitleField = "Default Title";
+        private static string DefaultTooltipField = "Default Tooltip";
+        private static string TooltipField = "Tooltip";
+        private static string IconField = "Icon";
+        public static string Tooltip = "Edit the following fields: {0}";
+        public static string Icon = "people/16x16/cubes_blue.png";
 
         private readonly Regex _titleRegex = new Regex("<title>(?<title>([^<]*))<title>");
 
