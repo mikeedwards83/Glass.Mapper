101720de3ef0a67c4a3a9ebd7f564b964f853f1b
Mike Edwards <mike@glass.lu>
2016-12-20 16:45:10 +0000

    Providing more information when a depth check exception is thrown

--- Source/Glass.Mapper/Pipelines/ObjectConstruction/Tasks/DepthCheck/ModelDepthCheck.cs
+++ Source/Glass.Mapper/Pipelines/ObjectConstruction/Tasks/DepthCheck/ModelDepthCheck.cs
@@ -9,9 +9,11 @@ namespace Glass.Mapper.Pipelines.ObjectConstruction.Tasks.DepthCheck
     public class ModelDepthCheck : AbstractObjectConstructionTask
     {
         const string Key = "EF601809-8711-4F34-AE8B-BDD814CE6FCC";
+        Stack<string> _models = new Stack<string>();
+
         public override void Execute(ObjectConstructionArgs args)
         {
-            using (new DepthCheck())
+            using (new DepthCheck(args.Configuration.Type))
             {
                 base.Execute(args);
             }
@@ -19,35 +21,40 @@ namespace Glass.Mapper.Pipelines.ObjectConstruction.Tasks.DepthCheck
 
         public class DepthCheck : IDisposable
         {
+            private readonly Type _type;
             const string Key = "1C983BD1-4642-48EA-BB41-C3CBB1B0376D";
 
-            public DepthCheck()
-                : this(8)
+            public DepthCheck(Type type)
+                : this(8, type)
             {
 
             }
-            public DepthCheck(int maxDepth)
+            public DepthCheck(int maxDepth, Type type)
             {
-                var current = 0;
+                _type = type;
+                Stack<string> stack = new Stack<string>();
                 if (ThreadData.Contains(Key))
                 {
-                    current = ThreadData.GetValue<int>(Key);
+                    stack = ThreadData.GetClass<Stack<string>>(Key);
+                    
                 }
-                current++;
-                if (current > maxDepth)
+                stack.Push(type.FullName);
+                if (stack.Count > maxDepth)
                 {
-                    throw new MapperStackException(Constants.Errors.ErrorLazyLoop);
+                    throw new MapperStackException(Constants.Errors.ErrorLazyLoop.Formatted(
+                        stack.Aggregate((x,y)=> "{0}{1}\n\r".Formatted(x,y))
+                        ));
                 }
-                ThreadData.SetValue(Key, current);
+                ThreadData.SetValue(Key, stack);
 
             }
             public void Dispose()
             {
                 if (ThreadData.Contains(Key))
                 {
-                    var current = ThreadData.GetValue<int>(Key);
-                    current--;
-                    ThreadData.SetValue(Key, current);
+                    var stack = ThreadData.GetClass<Stack<string>>(Key);
+                    stack.Pop();
+                    ThreadData.SetValue(Key, stack);
                 }
             }
         }
