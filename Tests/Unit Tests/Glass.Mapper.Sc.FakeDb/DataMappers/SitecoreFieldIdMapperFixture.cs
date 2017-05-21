94e32783a58058986ac6a9d63283e10da3742ec7
Mike Edwards <mike@glass.lu>
2017-05-21 09:14:10 +0100

    Unit tests for SetFieldValue in SitecoreFieldIdMapperFixture

--- Tests/Unit Tests/Glass.Mapper.Sc.FakeDb/DataMappers/SitecoreFieldIdMapperFixture.cs
+++ Tests/Unit Tests/Glass.Mapper.Sc.FakeDb/DataMappers/SitecoreFieldIdMapperFixture.cs
@@ -49,7 +49,38 @@ namespace Glass.Mapper.Sc.FakeDb.DataMappers
 
         #endregion
 
+        #region Method - GetField
 
+        [Test]
+        public void SetFieldValue_ContainsNull_ReturnsIDNull()
+        {
+            //Arrange
+            ID propertyValue = ID.Null;
+            var mapper = new SitecoreFieldIdMapper();
+
+            //Act
+            var result = mapper.SetFieldValue(propertyValue, null, null);
+
+            //Assert
+            Assert.AreEqual(ID.Null.ToString(), result);
+        }
+
+
+        [Test]
+        public void SetFieldValue_ContainsGuid_ReturnsID()
+        {
+            //Arrange
+            ID propertyValue = new ID("F55E52AC-A555-4E4D-BCD5-78F37FB1CEB1");
+            var mapper = new SitecoreFieldIdMapper();
+
+            //Act
+            var result = mapper.SetFieldValue(propertyValue, null, null);
+
+            //Assert
+            Assert.AreEqual(propertyValue.ToString(), result);
+        }
+
+        #endregion
 
         #region Stubs
 
