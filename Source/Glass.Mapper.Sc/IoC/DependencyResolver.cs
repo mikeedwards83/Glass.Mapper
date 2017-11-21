84935dd29889639b4269ae8e7492db36f8f9f720
Mike Edwards <mike@glass.lu>
2017-11-21 18:40:55 +0000

    Additional changes to remove locks from AbstractConfigFactory

--- Source/Glass.Mapper.Sc/IoC/DependencyResolver.cs
+++ Source/Glass.Mapper.Sc/IoC/DependencyResolver.cs
@@ -48,15 +48,6 @@ namespace Glass.Mapper.Sc.IoC
             return CacheManager();
         }
 
-        public void Finalise()
-        {
-            QueryParameterFactory.Finalise();
-            DataMapperResolverFactory.Finalise();
-            DataMapperFactory.Finalise();
-            ConfigurationResolverFactory.Finalise();
-            ObjectConstructionFactory.Finalise();
-            ObjectSavingFactory.Finalise();
-            ConfigurationMapFactory.Finalise();
-        }
+       
     }
 }
