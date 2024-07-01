b5640ea4917c0638c1fcf122aef98b3eecf5c926
girishsharma008 <128844261+girishsharma008@users.noreply.github.com>
2024-07-01 14:15:47 +0530

    reverted changes

--- Tests/Unit Tests/Glass.Mapper.Sc.FakeDb/ScVersionTest.cs
+++ Tests/Unit Tests/Glass.Mapper.Sc.FakeDb/ScVersionTest.cs
@@ -88,11 +88,5 @@ namespace Glass.Mapper.Sc.FakeDb
             Assert.True(true);
         }
 #endif
-#if SC104
-        public void Sc104()
-        {
-            Assert.True(true);
-        }
-#endif
     }
 }
